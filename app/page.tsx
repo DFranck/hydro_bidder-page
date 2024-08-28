702e730d707afcda571ac742bb325c15e96fdc14
MSalopek <matija.salopek994@gmail.com>
2024-08-28 16:07:28 +0200

    update form

--- app/page.tsx
+++ app/page.tsx
@@ -11,118 +11,149 @@ const howHydroWorksTiles = [
   {
     title: "Connect Wallet",
     description: "Correct your compatible wallet to the Hydro platform.",
-    icon: '/images/Wallet_Light.svg',
+    icon: "/images/Wallet_Light.svg",
   },
   {
     title: "Lock stATOM",
-    description: "Lock your stATOM for your chosen duration to get Voting Power..",
-    icon: '/images/Lock_Light.svg',
+    description:
+      "Lock your stATOM for your chosen duration to get Voting Power..",
+    icon: "/images/Lock_Light.svg",
   },
   {
     title: "Vote",
-    description: "Use your Voting Power to choose the bet projects that need liquidity and are offering you rewards.",
-    icon: '/images/Vote_Light.svg',
+    description:
+      "Use your Voting Power to choose the bet projects that need liquidity and are offering you rewards.",
+    icon: "/images/Vote_Light.svg",
   },
   {
     title: "Earn Rewards",
-    description: "Receive tribute each round from projects in exchange for providing them with liquidity.",
-    icon: '/images/Rewards_Light.svg',
+    description:
+      "Receive tribute each round from projects in exchange for providing them with liquidity.",
+    icon: "/images/Rewards_Light.svg",
   },
   {
     title: "Renew and Top-up",
-    description: "Sustain or increase your voting power by renewing or topping up your stATOM kickups",
-    icon: '/images/Renew_Light.svg',
+    description:
+      "Sustain or increase your voting power by renewing or topping up your stATOM kickups",
+    icon: "/images/Renew_Light.svg",
   },
   {
     title: "Repeat",
-    description: "Participate in new rounds and tranches to continue earning rewards from the projects on the Hydro platform.",
-    icon: '/images/Repeat_Light.svg',
+    description:
+      "Participate in new rounds and tranches to continue earning rewards from the projects on the Hydro platform.",
+    icon: "/images/Repeat_Light.svg",
   },
 ];
 
 const benefitsCheckList = [
-  'Participate in the growth of the Cosmos ecosystem',
-  'Provide attractive rewards for your project supporters',
-  'Access liquidity for your project',
-  'Gain exposure and visibility within the Cosmos community'
+  "Participate in the growth of the Cosmos ecosystem",
+  "Provide attractive rewards for your project supporters",
+  "Access liquidity for your project",
+  "Gain exposure and visibility within the Cosmos community",
 ];
 
 const howItWorksForProjectsTiles = [
   {
     title: "Get Allowlisted",
-    description: "Apply to get your project allowlisted on Hydro and start the process.",
-    icon: '/images/Graphic_List.svg',
+    description:
+      "Apply to get your project allowlisted on Hydro and start the process.",
+    icon: "/images/Graphic_List.svg",
   },
   {
     title: "Offer Tribute",
-    description: "Offer tribute to incentivize ATOM holders to support your project and provide liquidity.",
-    icon: '/images/Graphic_Offer.svg',
+    description:
+      "Offer tribute to incentivize ATOM holders to support your project and provide liquidity.",
+    icon: "/images/Graphic_Offer.svg",
   },
   {
     title: "Gain Voter Support",
-    description: "Attract voter support and access liquidity to your project’s growth.",
-    icon: '/images/Graphic_Vote.svg',
-  }
+    description:
+      "Attract voter support and access liquidity to your project’s growth.",
+    icon: "/images/Graphic_Vote.svg",
+  },
 ];
 
 type Tile = {
   title: string;
   description: string;
   icon: string;
-}
+};
 
 type TilesType = {
   tiles: Tile[];
-  size: 'small' | 'large';
-}
+  size: "small" | "large";
+};
 
 export default function Home() {
   const renderTiles = (data: TilesType) => {
     return (
       <div className={`grid grid-cols-3 gap-[60px] z-10 my-[60px] mx-[90px]`}>
-        {
-          data.tiles.map((tile, index) => {
-            return <div key={index} className={`flex w-[${data.size === 'small' ? '285px' : '330px'}] flex-col items-start gap-4 shrink-0 ${data.size === 'small' ? 'p-6' : ''} rounded-[10px]`}>
-              <Image src={tile.icon} alt={tile.title} width={data.size === 'small' ? 100 : 220} height={data.size === 'small' ? 100 : 220} />
+        {data.tiles.map((tile, index) => {
+          return (
+            <div
+              key={index}
+              className={`flex w-[${
+                data.size === "small" ? "285px" : "330px"
+              }] flex-col items-start gap-4 shrink-0 ${
+                data.size === "small" ? "p-6" : ""
+              } rounded-[10px]`}
+            >
+              <Image
+                src={tile.icon}
+                alt={tile.title}
+                width={data.size === "small" ? 100 : 220}
+                height={data.size === "small" ? 100 : 220}
+              />
               <h3>{tile.title}</h3>
               <p>{tile.description}</p>
             </div>
-          })
-        }
+          );
+        })}
       </div>
-    )
-  }
+    );
+  };
 
   const renderBenefits = () => {
     return (
       <div className="my-[60px]">
-        {
-          benefitsCheckList.map((item, index) => {
-            return (
-              <div key={index} className="flex flex-row items-center gap-4 shrink-0 py-[10px] rounded-[10px]">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                  <g clip-path="url(#clip0_174_196)">
-                    <path d="M6.90602 16.948C6.67402 16.948 6.44002 16.86 6.26402 16.682L0.266023 10.694C-0.0899765 10.34 -0.0899765 9.76401 0.266023 9.40801C0.620023 9.05201 1.19602 9.05201 1.55202 9.40801L6.91002 14.758L18.452 3.31601C18.808 2.96201 19.384 2.96401 19.738 3.32201C20.092 3.67801 20.09 4.25401 19.732 4.60801L7.54802 16.688C7.37002 16.864 7.14002 16.952 6.90802 16.952L6.90602 16.948Z" fill="#FFE1B8" />
-                  </g>
-                </svg>
-                <p>{item}</p>
-              </div>
-            )
-          })
-        }
+        {benefitsCheckList.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className="flex flex-row items-center gap-4 shrink-0 py-[10px] rounded-[10px]"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+                fill="none"
+              >
+                <g clipPath="url(#clip0_174_196)">
+                  <path
+                    d="M6.90602 16.948C6.67402 16.948 6.44002 16.86 6.26402 16.682L0.266023 10.694C-0.0899765 10.34 -0.0899765 9.76401 0.266023 9.40801C0.620023 9.05201 1.19602 9.05201 1.55202 9.40801L6.91002 14.758L18.452 3.31601C18.808 2.96201 19.384 2.96401 19.738 3.32201C20.092 3.67801 20.09 4.25401 19.732 4.60801L7.54802 16.688C7.37002 16.864 7.14002 16.952 6.90802 16.952L6.90602 16.948Z"
+                    fill="#FFE1B8"
+                  />
+                </g>
+              </svg>
+              <p>{item}</p>
+            </div>
+          );
+        })}
       </div>
-    )
-  }
+    );
+  };
 
   return (
-    <main className="flex min-h-screen flex-col bg-[#080815] bg-contain bg-no-repeat bg-[url('/images/AdobeStock_633966567.png')] relative after:z-[0] after:content-[''] after:absolute after:shadow-[0_0px_100px_300px_black] after:pointer-events-none after:top-[978px] after:inset-x-0" >
-
+    <main className="flex min-h-screen flex-col bg-[#080815] bg-contain bg-no-repeat bg-[url('/images/AdobeStock_633966567.png')] relative after:z-[0] after:content-[''] after:absolute after:shadow-[0_0px_100px_300px_black] after:pointer-events-none after:top-[978px] after:inset-x-0">
       <div className="ml-[88px]">
         <div className="mt-[155px]">
           <h1>Unlock the Power of Hydro</h1>
         </div>
         <p className="text-xl not-italic font-normal leading-[140%] w-[736px]">
-          Hydro is a decentralized platform that allows you to lock your ATOM tokens and earn rewards. Earn passive income, participate in ICS projects, and more.
+          Hydro is a decentralized platform that allows you to lock your ATOM
+          tokens and earn rewards. Earn passive income, participate in ICS
+          projects, and more.
         </p>
         <div className="flex gap-5 mt-[60px]">
           <Convert />
@@ -135,31 +166,39 @@ export default function Home() {
       </div>
       <div className="flex flex-col items-center">
         <div className="text-xl not-italic font-normal leading-[150%] z-[1]">
-          Understand the step-by-step process of participating in the Hydro ecosystem.
+          Understand the step-by-step process of participating in the Hydro
+          ecosystem.
         </div>
-        {renderTiles({ tiles: howHydroWorksTiles, size: 'small' })}
-        <Button type='secondary' style='filled' title="Get Started" />
+        {renderTiles({ tiles: howHydroWorksTiles, size: "small" })}
+        <Button type="secondary" style="filled" title="Get Started" />
       </div>
-      <HorizontalDivider style='mt-[60px] mb-[150px] mx-[60px]' />
+      <HorizontalDivider style="mt-[60px] mb-[150px] mx-[60px]" />
       <div className="ml-[112px] bg-contain bg-no-repeat bg-right bg-[url('/images/AdobeStock_856949849.png')] mix-blend-screen">
-        <p className="text-[#FFE1B8] slashed-zero text-base not-italic font-medium leading-[130%] tracking-[1.28px] uppercase">benefits</p>
+        <p className="text-[#FFE1B8] slashed-zero text-base not-italic font-medium leading-[130%] tracking-[1.28px] uppercase">
+          benefits
+        </p>
         <h2>Unlock the Power of Liquidity</h2>
         <div className="text-xl not-italic font-normal leading-[150%] w-[600px]  flex justify-center">
-          Hydro provides a unique opportunity to project to access liquidity and gain exposure, while rewarding ATOM holders for their participation.
+          Hydro provides a unique opportunity to project to access liquidity and
+          gain exposure, while rewarding ATOM holders for their participation.
         </div>
         {renderBenefits()}
-        <Button type='secondary' style='filled' title="Get Allowlisted" />
+        <Button type="secondary" style="filled" title="Get Allowlisted" />
       </div>
       <div className="flex flex-col items-center px-[90px]  bg-gradient-to-b from-[rgba(0,21,45,0.20)] to-[rgba(0,59,147,0.40)]">
-        <p className="text-[#FFE1B8] slashed-zero text-base not-italic font-medium leading-[130%] tracking-[1.28px] uppercase mt-[210px]">for projects</p>
+        <p className="text-[#FFE1B8] slashed-zero text-base not-italic font-medium leading-[130%] tracking-[1.28px] uppercase mt-[210px]">
+          for projects
+        </p>
         <h2 className="mt-[14px]">How it Works for Projects</h2>
         <p className="text-xl not-italic font-normal leading-[150%] w-[700px] mt-[14px] text-center">
-          Hydro provides a unique opportunity for projects to access liquidity and gain exposure, while rewarding ATOM holders for their participation through a multi-step process involving tribute auctions.
+          Hydro provides a unique opportunity for projects to access liquidity
+          and gain exposure, while rewarding ATOM holders for their
+          participation through a multi-step process involving tribute auctions.
         </p>
-        {renderTiles({ tiles: howItWorksForProjectsTiles, size: 'large' })}
+        {renderTiles({ tiles: howItWorksForProjectsTiles, size: "large" })}
         <div className="flex gap-5 mt-[60px] mb-[80px]">
-          <Button type='secondary' style='filled' title="Get Allowlisted" />
-          <Button type='secondary' style='outline' title="Read Docs" />
+          <Button type="secondary" style="filled" title="Get Allowlisted" />
+          <Button type="secondary" style="outline" title="Read Docs" />
         </div>
       </div>
       <Newsletter />
--- app/ui/modals/EditLockupDuration.tsx
+++ app/ui/modals/EditLockupDuration.tsx
@@ -1,117 +1,145 @@
-"use client"
-import { z } from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
+"use client";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form"
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogClose, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
-import { LockEntry } from "@/app/ts_types/HydroBase.types"
+import { LockEntry } from "@/app/ts_types/HydroBase.types";
 
 enum LockupPeriod {
-    "1m" = "1m",
-    "3m" = "3m",
-    "6m" = "6m",
-    "12m" = "12m",
+  "1m" = "1m",
+  "3m" = "3m",
+  "6m" = "6m",
+  "12m" = "12m",
 }
+
+const formSchema = z.object({
+  lockupPeriod: z.nativeEnum(LockupPeriod),
+  statom: z.string(),
+  hatom: z.coerce.number().min(0),
+});
 
 type EditLockupDurationProps = {
-    lockup: LockEntry
-    onEditLockup: (lockup: LockEntry) => void
-}
+  lockup: LockEntry;
+  onEditLockup: (lockup: LockEntry) => void;
+};
 
-export const EditLockupDuration = ({ lockup, onEditLockup }: EditLockupDurationProps) => {
-    const formSchema = z.object({
-        lockupPeriod: z.nativeEnum(LockupPeriod),
-        statom: z.string(),
-        hatom: z.coerce.number().min(0),
-    })
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            lockupPeriod: LockupPeriod["1m"],
-            statom: (parseFloat(lockup.funds.amount) / 1000000).toFixed(2),
-            hatom: 0,
-        },
-    })
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        onEditLockup({ ...lockup, ...values })
-    }
-    return (
-        <Dialog>
-            <DialogTrigger className="flex w-[90px] h-[40px] justify-center items-center gap-2.5 text-[#080815] text-center text-xl not-italic font-medium leading-[21px] bg-white px-6 rounded-[10px]">
-                Edit
-            </DialogTrigger>
-            <DialogContent className="bg-neutral-900 rounded-[10px] border-none w-[698px] p-12">
-                <DialogHeader className="pb-[34px]">
-                    <DialogTitle className="text-[32px] not-italic font-bold leading-[120%] tracking-[-0.4px] mb-[10px]">Edit Lockup Duration</DialogTitle>
-                </DialogHeader>
-                <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-5 mt-5 [&>*:last-child]:mt-[30px]">
-                        <FormField
-                            control={form.control}
-                            name="lockupPeriod"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <div className="gap-[56px] flex justify-start items-center">
-                                        <FormLabel className="text-sm not-italic font-normal leading-[120%] opacity-60 w-[100px]">New Lockup Time:</FormLabel>
-                                        <FormControl>
-                                            <ToggleGroup type="single" className="gap-[10px]" defaultValue={form.getValues("lockupPeriod")}>
-                                                {Object.entries(LockupPeriod).map(([value, label]) => (
-                                                    <ToggleGroupItem key={value} value={value} className="text-[#080815] text-center text-base not-italic font-medium leading-[21px] inline-flex h-[30px] justify-center items-center gap-2.5 shrink-0 bg-[rgba(255,255,255,0.40)] px-4 py-0 rounded-[100px]">
-                                                        {label}
-                                                    </ToggleGroupItem>
-                                                ))}
-                                            </ToggleGroup>
-                                        </FormControl>
-                                    </div>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="statom"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <div className="flex items-center gap-[56px]">
-                                        <FormLabel className="text-sm not-italic font-normal leading-[120%] opacity-60 w-[100px]">stATOMs:</FormLabel>
-                                        <p className="text-[#646464] text-xl not-italic font-medium leading-[21px]">{field.value}</p>
-                                    </div>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="hatom"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <div className="flex items-center gap-[56px]">
-                                        <FormLabel className="text-sm not-italic font-normal leading-[120%] opacity-60 w-[100px]">Updated Voting Power:</FormLabel>
-                                        <p className="text-xl not-italic font-medium leading-[21px]">{field.value}</p>
-                                    </div>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <Button variant='secondary'>Confirm</Button>
-                    </form>
-                </Form>
-                <DialogClose asChild>
-                    <Button type="button" variant="outline" className="w-full border rounded-[10px] border-solid border-white hover:bg-white hover:text-black">
-                        Cancel
-                    </Button>
-                </DialogClose>
-            </DialogContent>
-        </Dialog>
+export const EditLockupDuration = ({
+  lockup,
+  onEditLockup,
+}: EditLockupDurationProps) => {
+  console.log("LOCKUP", lockup);
 
-    )
-}
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      lockupPeriod: LockupPeriod["1m"],
+      statom: (parseFloat(lockup.funds.amount) / 1000000).toFixed(2),
+      hatom: 0,
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    onEditLockup({ ...lockup, ...values });
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger className="flex w-[90px] h-[40px] justify-center items-center gap-2.5 text-[#080815] text-center text-xl not-italic font-medium leading-[21px] bg-white px-6 rounded-[10px]">
+        Edit
+      </DialogTrigger>
+      <DialogContent className="bg-neutral-900 rounded-[10px] border-none w-[698px] p-12">
+        <DialogHeader className="pb-[34px]">
+          <DialogTitle className="text-[32px] not-italic font-bold leading-[120%] tracking-[-0.4px] mb-[10px]">
+            Edit Lockup Duration
+          </DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="grid gap-5 mt-5 [&>*:last-child]:mt-[30px]"
+          >
+            <FormField
+              control={form.control}
+              name="lockupPeriod"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="gap-[56px] flex justify-start items-center">
+                    <FormLabel className="text-sm not-italic font-normal leading-[120%] opacity-60 w-[100px]">
+                      New Lockup Time:
+                    </FormLabel>
+                    <FormControl>
+                      <ToggleGroup
+                        type="single"
+                        className="gap-[10px]"
+                        defaultValue={form.getValues("lockupPeriod")}
+                      >
+                        {Object.entries(LockupPeriod).map(([value, label]) => (
+                          <ToggleGroupItem
+                            key={value}
+                            value={value}
+                            className="text-[#080815] text-center text-base not-italic font-medium leading-[21px] inline-flex h-[30px] justify-center items-center gap-2.5 shrink-0 bg-[rgba(255,255,255,0.40)] px-4 py-0 rounded-[100px]"
+                          >
+                            {label}
+                          </ToggleGroupItem>
+                        ))}
+                      </ToggleGroup>
+                    </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <div className="flex items-center gap-[56px]">
+              <FormLabel className="text-sm not-italic font-normal leading-[120%] opacity-60 w-[100px]">
+                stATOMs:
+              </FormLabel>
+              <p className="text-[#646464] text-xl not-italic font-medium leading-[21px]">
+                {form.watch("statom")}
+              </p>
+            </div>
+
+            <div className="flex items-center gap-[56px]">
+              <FormLabel className="text-sm not-italic font-normal leading-[120%] opacity-60 w-[100px]">
+                Updated Voting Power:
+              </FormLabel>
+              <p className="text-xl not-italic font-medium leading-[21px]">
+                {form.watch("hatom")}
+              </p>
+            </div>
+
+            <Button variant="secondary" type="submit">
+              Confirm
+            </Button>
+          </form>
+        </Form>
+        <DialogClose asChild>
+          <Button
+            type="button"
+            variant="outline"
+            className="w-full border rounded-[10px] border-solid border-white hover:bg-white hover:text-black"
+          >
+            Cancel
+          </Button>
+        </DialogClose>
+      </DialogContent>
+    </Dialog>
+  );
+};
