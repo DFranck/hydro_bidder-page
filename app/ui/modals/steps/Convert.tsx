b10187d772a60a981924a94f1f64864c0c0df98d
mika48 <miodragmarcic@yahoo.com>
2024-08-28 10:00:16 +0200

    adjust Convert modal to match design

--- app/ui/modals/steps/Convert.tsx
+++ app/ui/modals/steps/Convert.tsx
@@ -1,5 +1,4 @@
 "use client"
-// import Button from "../../Button";
 import { Switch } from "../../../../components/ui/switch";
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
@@ -30,7 +29,7 @@ export const Convert = () => {
         nativeBalance: z.boolean(),
         lockupPeriod: z.nativeEnum(LockupPeriod),
         atom: z.coerce.number().min(0),
-        statom: z.coerce.number().min(0),
+        hatom: z.coerce.number().min(0),
     })
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -38,7 +37,7 @@ export const Convert = () => {
             nativeBalance: false,
             lockupPeriod: LockupPeriod["1m"],
             atom: 0,
-            statom: 0,
+            hatom: 0,
         },
     })
     function onSubmit(values: z.infer<typeof formSchema>) {
@@ -51,7 +50,7 @@ export const Convert = () => {
             </DialogTrigger>
             <DialogContent className="bg-neutral-900 rounded-[10px] border-none w-[698px] p-12">
                 <DialogHeader className="pb-[34px]">
-                    <DialogTitle className="text-[32px] not-italic font-bold leading-[120%] tracking-[-0.4px] mb-[10px]">Step 1: Convert ATOM to stATOM</DialogTitle>
+                    <DialogTitle className="text-[32px] not-italic font-bold leading-[120%] tracking-[-0.4px] mb-[10px]">Step 1: Lock up your ATOM for hATOM</DialogTitle>
                     <DialogDescription className="text-white/50 text-xl not-italic font-normal leading-[150%]">
                         Stake your ATOM tokens in exchange for stATOM which you can deploy around the ecosystem. You can liquid stake half of your balance, if you’re going to LP.
                     </DialogDescription>
@@ -114,14 +113,14 @@ export const Convert = () => {
                         />
                         <FormField
                             control={form.control}
-                            name="statom"
+                            name="hatom"
                             render={({ field }) => (
                                 <FormItem>
                                     <FormDescription>
                                         What You’ll Get
                                     </FormDescription>
                                     <div className="flex justify-between items-center">
-                                        <FormLabel className="text-xl not-italic font-normal leading-[150%]">stATOM:</FormLabel>
+                                        <FormLabel className="text-xl not-italic font-normal leading-[150%]">hATOM:</FormLabel>
                                         <FormControl>
                                             <Input type="number" className="flex w-[505px] h-10 items-center gap-2.5 shrink-0 border opacity-60 px-4 py-0 rounded-[10px] border-solid border-white bg-transparent" {...field} />
                                         </FormControl>
