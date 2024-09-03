3cc40c4d70113ef6fb5ebdf9ae7874d4bb14ef2e
mika48 <miodragmarcic@yahoo.com>
2024-09-03 09:49:31 +0200

    fix build

--- app/ui/Header.tsx
+++ app/ui/Header.tsx
@@ -1,16 +1,17 @@
 'use client'
 import Image from "next/image"
 import { Wallet } from "@/components/wallet/Wallet"
-import Link from "next/link"
-import { usePathname } from 'next/navigation'
 import Navigation from "./Navigation"
+import { Suspense } from "react"
 
 export const Header = () => {
     return (
         <div className="max-w-[1440px] mx-auto bg-black p-[60px] z-10 w-full items-center justify-between font-mono text-sm lg:flex">
             <Image src={'/images/logo.svg'} alt='twitter' width={220} height={45} />
             <div className="flex flex-row items-center justify-between gap-6">
-                <Navigation />
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Navigation />
+                </Suspense>
                 <Wallet />
             </div>
         </div>
