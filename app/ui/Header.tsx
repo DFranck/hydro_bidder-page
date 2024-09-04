859fd1462700de51817d0539cb85a4e9701c700c
mika48 <miodragmarcic@yahoo.com>
2024-09-04 12:13:53 +0200

    add link to homepage on logo image in header and footer

--- app/ui/Header.tsx
+++ app/ui/Header.tsx
@@ -3,11 +3,14 @@ import Image from "next/image"
 import { Wallet } from "@/components/wallet/Wallet"
 import Navigation from "./Navigation"
 import { Suspense } from "react"
+import Link from "next/link"
 
 export const Header = () => {
     return (
         <div className="max-w-[1440px] mx-auto bg-black p-[60px] z-10 w-full items-center justify-between font-mono text-sm lg:flex">
-            <Image src={'/images/logo.svg'} alt='twitter' width={220} height={45} />
+            <Link href={'/'}>
+                <Image src={'/images/logo.svg'} alt='twitter' width={220} height={45} />
+            </Link>
             <div className="flex flex-row items-center justify-between gap-6">
                 <Suspense fallback={<div>Loading...</div>}>
                     <Navigation />
