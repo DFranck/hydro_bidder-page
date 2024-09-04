1853da5b1b05e89c9c356e7d3bbdec895396abe9
mika48 <miodragmarcic@yahoo.com>
2024-09-04 12:13:53 +0200

    add link to homepage on logo image in header and footer

--- app/ui/Footer.tsx
+++ app/ui/Footer.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export const Footer = () => {
     return (
         <div className="px-[60px] py-[33px] bg-[#303132] z-10 w-full items-center justify-between font-mono text-sm lg:flex">
-            <Image src={'/images/logo.svg'} alt='twitter' width={220} height={45} />
+            <Link href={'/'}>
+                <Image src={'/images/logo.svg'} alt='twitter' width={220} height={45} />
+            </Link>
             <div className="flex flex-row items-center justify-between gap-5">
                 <p className="text-[rgba(255,255,255,0.60)] text-right text-base italic font-normal leading-[160%]">
                     Built by Informal Systems
