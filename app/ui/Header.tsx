13c2e6b33916ca843b7a2d5129b047a9f27bd326
MSalopek <matija.salopek994@gmail.com>
2024-09-13 13:09:42 +0200

    update layouts

--- app/ui/Header.tsx
+++ app/ui/Header.tsx
@@ -7,7 +7,7 @@ import Link from "next/link"
 
 export const Header = () => {
     return (
-        <div className="max-w-[1440px] mx-auto bg-black p-[60px] z-10 w-full items-center justify-between font-mono text-sm lg:flex font-inter">
+        <div className="max-w-[1440px] mx-auto bg-black p-[60px] z-10 w-full items-center justify-between font-mono text-sm lg:flex">
             <Link href={'/'}>
                 <Image src={'/images/logo.svg'} alt='twitter' width={220} height={45} />
             </Link>
