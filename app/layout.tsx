da5a4d0b9610a2a6e98cda7911355123a6ccd2c8
MSalopek <matija.salopek994@gmail.com>
2024-09-13 13:09:42 +0200

    update layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,50 +1,34 @@
-'use client';
-import localFont from 'next/font/local'
-import "./globals.css";
-import { GoogleTagManager } from "@next/third-parties/google";
+import "./globals.css"
+import { GoogleTagManager } from "@next/third-parties/google"
 
-import { Header } from "./ui/Header";
-import { WalletHandler } from "./wallet";
-import { Toaster } from "@/components/ui/toaster";
-import Script from "next/script";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { useState } from 'react';
+import Script from "next/script"
+import { inter } from "@/lib/font"
+import { ClientHandler } from "./clientLayout"
 
-const inter = localFont({
-  src: "../public/fonts/Inter/Inter-VariableFont_opsz,wght.ttf",
-  display: "swap",
-  declarations: [
-    {
-      prop: "font-feature-settings",
-      value: '"zero" 1',
-    },
-  ],
-});
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "Hydro - Cosmos Hub Liquidity Platform",
+    description:
+        "Hydro is a Cosmos Hub liquidity platform that allows you to lock your ATOM and participate in the growth of the Cosmos ecosystem.",
+}
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode
 }>) {
-  const [queryClient] = useState(() => new QueryClient());
-
-  return (
-    <html lang="en">
-      {/* <GoogleTagManager gtmId="G-NZ1F6WL2PM" />
+    return (
+        <html lang="en">
+            {/* <GoogleTagManager gtmId="G-NZ1F6WL2PM" />
       <Script
         type="text/javascript"
         src="https://www.bugherd.com/sidebarv2.js?apikey=mdyh8j9rijiqijf1qow8tw"
         async
       /> */}
-      <body className={`${inter.className} text-white bg-black`}>
-        <QueryClientProvider client={queryClient}>
-          <WalletHandler>
-            <Header />
-            {children}
-            <Toaster />
-          </WalletHandler>
-        </QueryClientProvider>
-      </body>
-    </html >
-  );
+            <body className={`${inter.className} text-white bg-black`}>
+                <ClientHandler>{children}</ClientHandler>
+            </body>
+        </html>
+    )
 }
