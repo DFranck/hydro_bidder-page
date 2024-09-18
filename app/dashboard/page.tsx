1128d8b7810fdf8005c4c4babdafdf9aa0f30c17
MSalopek <matija.salopek994@gmail.com>
2024-09-18 15:14:46 +0200

    connect lockup dash with real data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,19 +1,14 @@
 import { Suspense } from "react"
-import { fetchDashboardData } from "./dashboardFetch"
 import LockupsTable from "./lockupsTable"
 import { DashboardTopModules } from "./TopModules"
 
 export default async function Page() {
-    const { currentProposalTranches, globalState } = await fetchDashboardData()
     return (
         <Suspense fallback={<div>Loading...</div>}>
             <div className="max-w-7xl mx-auto">
                 <DashboardTopModules />
                 <div className="pt-10 pb-44">
-                    <LockupsTable
-                        currentProposalTranches={currentProposalTranches}
-                        globalState={globalState}
-                    />
+                    <LockupsTable />
                 </div>
             </div>
         </Suspense>
