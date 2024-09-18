76e7ab6e570765435fb7171ff31b84b633c345cf
MSalopek <matija.salopek994@gmail.com>
2024-09-18 17:40:03 +0200

    cleanup deployed proposals (stil broken)

--- app/deployed-proposals/component.tsx
+++ app/deployed-proposals/component.tsx
@@ -1,87 +1,165 @@
-'use client'
-import { useEffect, useState } from "react";
-import { DataTable, makeProposalColumnDef } from "../ui/proposalTable";
-import { ProposalListTopModules } from "../dashboard/TopModules";
-import { Proposal, Tranche } from "../ts_types/HydroBase.types";
-import { Tribute } from "../ts_types/TributeBase.types";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import Link from "next/link";
-import { GlobalState } from "../types";
-import { fetchDashboardData } from "../dashboard/dashboardFetch";
+"use client"
+import { useEffect, useState } from "react"
+import { DataTable, makeProposalColumnDef } from "../ui/proposalTable"
+import { Proposal, Tranche } from "../ts_types/HydroBase.types"
+import { Tribute } from "../ts_types/TributeBase.types"
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+import Link from "next/link"
+import { GlobalState } from "../types"
 
 const DeployedLiquidity = ({
     lastProposalTranches,
     lastProposalTributes,
-    globalState
+    globalState,
 }: {
-    lastProposalTranches?: Map<number, Proposal[]>,
-    lastProposalTributes?: Map<number, Tribute[]>,
+    lastProposalTranches?: Map<number, Proposal[]>
+    lastProposalTributes?: Map<number, Tribute[]>
     globalState: GlobalState | { tranches: Tranche[] }
 }) => {
-    const [currentTranche, setCurrentTranche] = useState(0);
+    const [currentTranche, setCurrentTranche] = useState(0)
 
     return (
-        <div className="px-[90px] pb-[90px] max-w-[1440px] mx-auto">
-            <ProposalListTopModules />
-            <div className="mt-14 relative">
-                <div className="p-5 absolute flex flex-row gap-[18px] justify-between items-center w-[380px] border rounded-[40px] border-solid border-[#FFE1B8] right-0">
-                    <Button variant='ghost' className="hover:bg-transparent text-[#E4B472]" size="icon" onClick={() => setCurrentTranche(currentTranche > 1 ? currentTranche - 1 : globalState.tranches.length)}>
-                        <Image src={'/images/Vector3.svg'} alt='tranches-left' width={24} height={40} />
-                    </Button>
-                    <p className="text-[32px] not-italic font-normal leading-[120%] tracking-[-0.4px]  text-[#E4B472]">{`TRANCHE ${currentTranche}/${globalState.tranches.length}`}</p>
-                    <Button variant="ghost" className="hover:bg-transparent  text-[#E4B472]" size="icon" onClick={() => setCurrentTranche(currentTranche < globalState.tranches.length ? currentTranche + 1 : 1)}>
-                        <Image src={'/images/Vector4.svg'} alt='tranches-right' width={24} height={40} />
-                    </Button>
-                </div>
-                <h3 className="pb-5">Actively Deployed Proposals</h3>
-                <p className="text-xl not-italic font-normal leading-[150%]">Winning proposals from previous rounds that are currently deployed</p>
-                {lastProposalTranches && lastProposalTributes && lastProposalTranches.get(currentTranche) ? (
-                    <DataTable
-                        columns={[
-                            {
-                                accessorKey: "title",
-                                header: "",
-                                cell: ({ row }) => {
-                                    return (<div className="flex flex-col">
-                                        <p className="text-xl not-italic font-bold leading-[150%]">{row.original.proposal.title}</p>
-                                    </div>
-                                    )
-                                },
-                            },
-                            {
-                                accessorKey: "tribute",
-                                header: () => <div className="text-center">Tribute Amount</div>,
-                                cell: ({ row }) => <div className="text-center">{row.original.summedTributes.map((tribute, index) =>
-                                    <div key={index} className="text-center">
-                                        {`${(tribute.amount / 1000000).toFixed(2)} ${tribute.denom.length > 20 ? tribute.denom.slice(0, 17) + '...' : tribute.denom}`}
-                                    </div>)
-                                }</div>,
-                            },
-                            {
-                                accessorKey: "votingPowerPercent",
-                                header: () => <div className="text-center">Voting Power %</div>,
-                                cell: ({ row }) => <div className="text-center">{row.original.proposal.percentage}</div>,
-                            },
-                            {
-                                accessorKey: "link",
-                                header: "",
-                                cell: ({ row }) => {
-                                    return (<Link href={`/deployed-proposals/${row.original.proposal.proposal_id}`}><Button className="bg-[#0061FF]">View Proposal</Button></Link>)
-                                }
-                            }
-                        ]}
-                        data={
-                            (lastProposalTranches.get(currentTranche) || [])
-                                .map((proposal) => makeProposalColumnDef(proposal, lastProposalTributes.get(proposal.proposal_id)!))
-                        }
+        <div className="mt-14 relative">
+            <div className="p-5 absolute flex flex-row gap-[18px] justify-between items-center w-[380px] border rounded-[40px] border-solid border-[#FFE1B8] right-0">
+                <Button
+                    variant="ghost"
+                    className="hover:bg-transparent text-[#E4B472]"
+                    size="icon"
+                    onClick={() =>
+                        setCurrentTranche(
+                            currentTranche > 1
+                                ? currentTranche - 1
+                                : globalState.tranches.length
+                        )
+                    }
+                >
+                    <Image
+                        src={"/images/Vector3.svg"}
+                        alt="tranches-left"
+                        width={24}
+                        height={40}
                     />
-                ) : (
-                    <div className="text-center py-8 text-xl">There are no deployed proposals yet.</div>
-                )}
+                </Button>
+                <p className="text-[32px] not-italic font-normal leading-[120%] tracking-[-0.4px]  text-[#E4B472]">{`TRANCHE ${currentTranche}/${globalState.tranches.length}`}</p>
+                <Button
+                    variant="ghost"
+                    className="hover:bg-transparent  text-[#E4B472]"
+                    size="icon"
+                    onClick={() =>
+                        setCurrentTranche(
+                            currentTranche < globalState.tranches.length
+                                ? currentTranche + 1
+                                : 1
+                        )
+                    }
+                >
+                    <Image
+                        src={"/images/Vector4.svg"}
+                        alt="tranches-right"
+                        width={24}
+                        height={40}
+                    />
+                </Button>
             </div>
+            <h3 className="pb-5">Actively Deployed Proposals</h3>
+            <p className="text-xl not-italic font-normal leading-[150%]">
+                Winning proposals from previous rounds that are currently
+                deployed
+            </p>
+            {lastProposalTranches &&
+            lastProposalTributes &&
+            lastProposalTranches.get(currentTranche) ? (
+                <DataTable
+                    columns={[
+                        {
+                            accessorKey: "title",
+                            header: "",
+                            cell: ({ row }) => {
+                                return (
+                                    <div className="flex flex-col">
+                                        <p className="text-xl not-italic font-bold leading-[150%]">
+                                            {row.original.proposal.title}
+                                        </p>
+                                    </div>
+                                )
+                            },
+                        },
+                        {
+                            accessorKey: "tribute",
+                            header: () => (
+                                <div className="text-center">
+                                    Tribute Amount
+                                </div>
+                            ),
+                            cell: ({ row }) => (
+                                <div className="text-center">
+                                    {row.original.summedTributes.map(
+                                        (tribute, index) => (
+                                            <div
+                                                key={index}
+                                                className="text-center"
+                                            >
+                                                {`${(
+                                                    tribute.amount / 1000000
+                                                ).toFixed(2)} ${
+                                                    tribute.denom.length > 20
+                                                        ? tribute.denom.slice(
+                                                              0,
+                                                              17
+                                                          ) + "..."
+                                                        : tribute.denom
+                                                }`}
+                                            </div>
+                                        )
+                                    )}
+                                </div>
+                            ),
+                        },
+                        {
+                            accessorKey: "votingPowerPercent",
+                            header: () => (
+                                <div className="text-center">
+                                    Voting Power %
+                                </div>
+                            ),
+                            cell: ({ row }) => (
+                                <div className="text-center">
+                                    {row.original.proposal.percentage}
+                                </div>
+                            ),
+                        },
+                        {
+                            accessorKey: "link",
+                            header: "",
+                            cell: ({ row }) => {
+                                return (
+                                    <Link
+                                        href={`/deployed-proposals/${row.original.proposal.proposal_id}`}
+                                    >
+                                        <Button className="bg-[#0061FF]">
+                                            View Proposal
+                                        </Button>
+                                    </Link>
+                                )
+                            },
+                        },
+                    ]}
+                    data={(lastProposalTranches.get(currentTranche) || []).map(
+                        (proposal) =>
+                            makeProposalColumnDef(
+                                proposal,
+                                lastProposalTributes.get(proposal.proposal_id)!
+                            )
+                    )}
+                />
+            ) : (
+                <div className="text-center py-8 text-xl">
+                    There are no deployed proposals yet.
+                </div>
+            )}
         </div>
     )
 }
 
-export default DeployedLiquidity;
+export default DeployedLiquidity
--- app/deployed-proposals/page.tsx
+++ app/deployed-proposals/page.tsx
@@ -1,19 +1,65 @@
-import { fetchDashboardData } from "../dashboard/dashboardFetch";
-import DeployedLiquidity from "./component";
+import DeployedLiquidity from "./component"
+import { ATOM_PRICE_URL } from "../config"
+import { fetchDashboardData } from "../dashboard/getData"
+import { ProposalListTopModules } from "../voting-proposals/TopModules"
 
-const Page = async () => {
+export default async function DeployedProposalPage() {
     const {
         globalState,
         lastProposalTranches,
         lastProposalTributes,
-    } = await fetchDashboardData();
+        currentProposalTributes,
+        currentRoundEnd,
+    } = await fetchDashboardData()
+
+    let atomPrice = 0
+    try {
+        // responds with: { cosmos: { usd: 4.13 } }
+        const res = await fetch(ATOM_PRICE_URL).then((res) => res.json())
+        atomPrice = res["cosmos"]["usd"]
+    } catch {
+        console.log("failed to fetch atom price data")
+    }
+
+    const allTributesCoins = Array.from(currentProposalTributes.values())
+        .flat()
+        .reduce((acc, tribute) => {
+            const { denom, amount } = tribute.funds
+            if (!acc[denom]) {
+                acc[denom] = BigInt(0)
+            }
+            acc[denom] += BigInt(amount)
+            return acc
+        }, {} as Record<string, bigint>)
+
+    // gets symbol/pretty name for each denom
+    const resolvedTributes = Object.entries(allTributesCoins).reduce(
+        (acc, [denom, amount]) => {
+            const asset = NEUTRON_ASSETS.assets.find(
+                (asset) =>
+                    asset.base.toLowerCase() === denom.toLowerCase() ||
+                    (denom.toLowerCase().startsWith("ibc/") &&
+                        asset.base.toLowerCase() === denom.toLowerCase())
+            )
+            const symbol = asset ? asset.symbol : denom
+            acc[symbol] = amount
+            return acc
+        },
+        {} as Record<string, bigint>
+    )
 
     return (
-        <DeployedLiquidity
-            lastProposalTranches={lastProposalTranches}
-            lastProposalTributes={lastProposalTributes}
-            globalState={globalState}
-        />
+        <div className="pb-44 max-w-7xl mx-auto px-5 lg:px-0">
+            <ProposalListTopModules
+                lockedAtom={globalState.totalLockedTokens}
+                roundEnd={currentRoundEnd}
+                atomPrice={atomPrice}
+            />
+            <DeployedLiquidity
+                lastProposalTranches={lastProposalTranches}
+                lastProposalTributes={lastProposalTributes}
+                globalState={globalState}
+            />
+        </div>
     )
 }
-export default Page;
