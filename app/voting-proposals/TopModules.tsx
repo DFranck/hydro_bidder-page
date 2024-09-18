f92e977b50e3576c6b7f31d5eadc1498569e57c9
MSalopek <matija.salopek994@gmail.com>
2024-09-18 18:32:24 +0200

    fix voting page

--- app/voting-proposals/TopModules.tsx
+++ app/voting-proposals/TopModules.tsx
@@ -1,5 +1,14 @@
 import { Timestamp } from "../ts_types/HydroBase.types"
 
+const getRoundEndText = (roundEnd: Timestamp) => {
+    const now = new Date()
+    const end = new Date(parseInt(roundEnd) / 1e6)
+    const diff = end.getTime() - now.getTime()
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24))
+    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    return `${days}:${hours.toString().padStart(2, "0")}`
+}
+
 export const ProposalListTopModules = ({
     lockedAtom,
     roundEnd,
@@ -11,12 +20,13 @@ export const ProposalListTopModules = ({
 }) => {
     const bgColor =
         "bg-transparent bg-[linear-gradient(180deg,rgba(0,59,147,0.30)_0%,rgba(0,97,255,0.70)_100%)]"
+
     return (
-        <div className="flex flex-row justify-between bg-transparent">
-            <div
-                className={`flex w-[380px] h-[206px] flex-col shrink-0 p-6 rounded-[10px] ${bgColor}`}
-            >
-                <h3 className={`pb-4 text-white whitespace-pre-wrap`}>
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 justify-between bg-transparent">
+            <div className={`flex flex-col p-6 rounded-xl ${bgColor}`}>
+                <h3
+                    className={`pb-4 text-white whitespace-pre-wrap text-2xl lg:text-4xl`}
+                >
                     Current Round <br />
                     Tribute Value
                 </h3>
@@ -35,33 +45,17 @@ export const ProposalListTopModules = ({
                     USDC EQUIVALENT
                 </p>
             </div>
-            <div
-                className={`flex w-[380px] h-[206px] flex-col shrink-0 p-6 rounded-[10px] ${bgColor}`}
-            >
-                <h3 className={`pb-4 text-white whitespace-pre-wrap`}>
+            <div className={`flex flex-col p-6 rounded-xl ${bgColor}`}>
+                <h3
+                    className={`pb-4 text-white whitespace-pre-wrap text-2xl lg:text-4xl`}
+                >
                     Current Round <br />
                     Time Remaining
                 </h3>
                 <p
                     className={`text-[#E4B472] slashed-zero text-5xl not-italic font-bold leading-[124.7%] tracking-[-1.296px]`}
                 >
-                    {roundEnd
-                        ? (() => {
-                              const now = new Date()
-                              const end = new Date(parseInt(roundEnd) / 1e6)
-                              const diff = end.getTime() - now.getTime()
-                              const days = Math.floor(
-                                  diff / (1000 * 60 * 60 * 24)
-                              )
-                              const hours = Math.floor(
-                                  (diff % (1000 * 60 * 60 * 24)) /
-                                      (1000 * 60 * 60)
-                              )
-                              return `${days}:${hours
-                                  .toString()
-                                  .padStart(2, "0")}`
-                          })()
-                        : "0:00"}
+                    {roundEnd ? getRoundEndText(roundEnd) : "0:00"}
                 </p>
                 <p
                     className={`text-[#FFE1B8] slashed-zero text-base not-italic font-medium leading-[130%] uppercase`}
@@ -69,10 +63,10 @@ export const ProposalListTopModules = ({
                     DAYS: HOURS
                 </p>
             </div>
-            <div
-                className={`flex w-[380px] h-[206px] flex-col shrink-0 p-6 rounded-[10px] ${bgColor}`}
-            >
-                <h3 className={`pb-4 text-white whitespace-pre-wrap`}>
+            <div className={`flex flex-col p-6 rounded-xl ${bgColor}`}>
+                <h3
+                    className={`pb-4 text-white whitespace-pre-wrap text-2xl lg:text-4xl`}
+                >
                     Total Locked
                     <br />
                     ATOM
