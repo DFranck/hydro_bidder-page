6ce3b6e4890d6a75d69a09664faf1ae28bca72bf
Aaron Wright <aaronmw@gmail.com>
2024-09-30 15:42:00 -0400

    Flip logic for showing lock atom banner

--- components/Header.tsx
+++ components/Header.tsx
@@ -83,7 +83,7 @@ export const Header = () => {
                     <Navigation />
                 </div>
             </div>
-            {!showLockATOMBanner && (
+            {showLockATOMBanner && (
                 <div
                     className={twMerge(
                         `
