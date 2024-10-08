1bb86f9b0dae77b249d4947dadc35231cd785af1
Aaron Wright <aaronmw@gmail.com>
2024-10-08 17:09:06 +0200

    Add tooltips to table headings

--- components/TooltipIcon.tsx
+++ components/TooltipIcon.tsx
@@ -1,35 +1,50 @@
 import { Info } from "lucide-react"
 import { ReactNode } from "react"
+import { twMerge } from "tailwind-merge"
 
-export function TooltipIcon({ children }: { children: ReactNode }) {
+export function TooltipIcon({
+    children,
+    classNamesForTooltip,
+}: {
+    children: ReactNode
+    classNamesForTooltip?: string
+}) {
     return (
-        <div className="group relative">
+        <div className="group relative" tabIndex={0}>
             <Info className="inline-block" size={14} />
             <div
-                className="
-                    pointer-events-none
-                    absolute
-                    left-1/2
-                    top-full
-                    z-50
-                    w-56
-                    -translate-x-1/2
-                    translate-y-full
-                    rounded-sm
-                    border
-                    border-palette-beige
-                    bg-palette-text
-                    px-3
-                    py-1
-                    text-sm
-                    font-normal
-                    text-white
-                    opacity-0
-                    transition-all
-                    group-hover:pointer-events-auto
-                    group-hover:translate-y-0
-                    group-hover:opacity-100
-                "
+                className={twMerge(
+                    `
+                        pointer-events-none
+                        absolute
+                        left-1/2
+                        top-full
+                        z-50
+                        w-56
+                        -translate-x-1/2
+                        translate-y-full
+                        whitespace-normal
+                        rounded-sm
+                        border
+                        border-palette-beige
+                        bg-palette-text
+                        px-3
+                        py-1
+                        text-left
+                        text-sm
+                        font-normal
+                        text-white
+                        opacity-0
+                        transition-all
+                        group-hover:pointer-events-auto
+                        group-hover:translate-y-0
+                        group-hover:opacity-100
+                        group-focus:pointer-events-auto
+                        group-focus:translate-y-0
+                        group-focus:opacity-100
+                    `,
+                    classNamesForTooltip
+                )}
             >
                 {children}
             </div>
