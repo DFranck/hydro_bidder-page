2486a5a96dda5cbe0cbbde3b2d1938f9515956d1
MSalopek <matija.salopek994@gmail.com>
2024-10-09 11:21:40 +0200

    add bugherd

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,11 @@ export default function RootLayout({
                     gtag('config', 'G-NZ1F6WL2PM')
                 `}
             </Script>
+            <Script
+                type="text/javascript"
+                src="https://www.bugherd.com/sidebarv2.js?apikey=mdyh8j9rijiqijf1qow8tw"
+                async
+            ></Script>
             <body className={`${inter.className} relative overflow-x-hidden`}>
                 {/* <div className="bg-gradient-to-b from-black via-[#010006] to-[#001C47] p-5 h-screen w-screen fixed top-0 left-0 -z-10"></div> */}
                 <div
