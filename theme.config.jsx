77a39f5717882ce3368cfaa577e6572307806b5f
Aaron Wright <aaronmw@gmail.com>
2024-10-15 12:39:30 -0400

    Hide Nextra from titles in a dumb way

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,5 +1,7 @@
 import "@/app/globals.css"
 import Image from "next/image"
+import { useConfig } from "nextra-theme-docs"
+import { useEffect } from "react"
 
 const themeConfig = {
     logo: (
@@ -24,25 +26,43 @@ const themeConfig = {
     footer: {
         component: null,
     },
-    head: (
-        <>
-            <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1.0"
-            />
-            <link
-                rel="icon"
-                type="image/png"
-                sizes="32x32"
-                href="/favicon.png"
-            />
-            <meta property="og:title" content="Hydro Documentation" />
-            <meta
-                property="og:description"
-                content="Hydro is a Cosmos Hub liquidity platform that allows you to lock your ATOM and participate in the growth of the Cosmos ecosystem."
-            />
-        </>
-    ),
+    head() {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const { frontMatter } = useConfig()
+        const title = frontMatter.title || "Hydro Documentation"
+        const description =
+            frontMatter.description ||
+            "Hydro is a Cosmos Hub liquidity platform that allows you to lock your ATOM and participate in the growth of the Cosmos ecosystem."
+
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        useEffect(() => {
+            const interval = setInterval(() => {
+                window.document.title = window.document.title.replace(
+                    " – Nextra",
+                    ""
+                )
+            }, 250)
+
+            return () => clearInterval(interval)
+        }, [])
+
+        return (
+            <>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon.png"
+                />
+                <meta property="og:title" content={title} />
+                <meta property="og:description" content={description} />
+            </>
+        )
+    },
     // ... other theme options
 }
 
