128c74875b1a9577c246d02e33adf335c2cdca6b
MSalopek <matija.salopek994@gmail.com>
2024-10-15 11:41:59 +0200

    fix: use topN and roundProposals data for full prop info

--- hooks/hooks.ts
+++ hooks/hooks.ts
@@ -299,19 +299,37 @@ export const fetchProposals = async (
         client,
         HYDRO_CONTRACT_ADDRESS
     )
+    // query all proposals and enrich with topNProposals percentage data
+    // return proposals sorted by percentage in descending order
     const response = await unstable_cache(
         async () => {
-            return hydroQueryClient.roundProposals({
+            const roundProposals = await hydroQueryClient.roundProposals({
                 limit: 20,
                 roundId,
                 startFrom: 0,
                 trancheId,
             })
-            // return hydroQueryClient.topNProposals({
-            //     numberOfProposals: 20,
-            //     roundId,
-            //     trancheId,
-            // })
+            const topNProposals = await hydroQueryClient.topNProposals({
+                numberOfProposals: 20,
+                roundId,
+                trancheId,
+            })
+            const enrichedProposals = roundProposals.proposals
+                .map((proposal) => {
+                    const matchingTopProposal = topNProposals.proposals.find(
+                        (topProposal) =>
+                            topProposal.proposal_id === proposal.proposal_id
+                    )
+                    return {
+                        ...proposal,
+                        percentage: matchingTopProposal
+                            ? matchingTopProposal.percentage
+                            : proposal.percentage,
+                    }
+                })
+                .sort((a, b) => parseInt(b.percentage) - parseInt(a.percentage))
+
+            return { proposals: enrichedProposals }
         },
         ["topNProposals", roundId.toString(), trancheId.toString()],
         { revalidate: CACHE_REVALIDATE_SECONDS }
