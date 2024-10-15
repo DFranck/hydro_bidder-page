e9763920ffa6555be50f555c50863dc20af2dee1
MSalopek <matija.salopek994@gmail.com>
2024-10-15 15:53:04 +0200

    dev: enable ledger support (use amino signers)

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,23 +19,27 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <Script
-                async
-                src="https://www.googletagmanager.com/gtag/js?id=G-NZ1F6WL2PM"
-            ></Script>
-            <Script id="google-analytics" strategy="afterInteractive">
-                {`
+            {process.env.NODE_ENV === "production" && (
+                <>
+                    <Script
+                        async
+                        src="https://www.googletagmanager.com/gtag/js?id=G-NZ1F6WL2PM"
+                    ></Script>
+                    <Script id="google-analytics" strategy="afterInteractive">
+                        {`
                     window.dataLayer = window.dataLayer || []
                     function gtag(){dataLayer.push(arguments)}
                     gtag('js', new Date())
                     gtag('config', 'G-NZ1F6WL2PM')
-                `}
-            </Script>
-            <Script
-                type="text/javascript"
-                src="https://www.bugherd.com/sidebarv2.js?apikey=mdyh8j9rijiqijf1qow8tw"
-                async
-            ></Script>
+                    `}
+                    </Script>
+                    <Script
+                        type="text/javascript"
+                        src="https://www.bugherd.com/sidebarv2.js?apikey=mdyh8j9rijiqijf1qow8tw"
+                        async
+                    ></Script>
+                </>
+            )}
             <body className={`${inter.className} relative overflow-x-hidden`}>
                 {/* <div className="bg-gradient-to-b from-black via-[#010006] to-[#001C47] p-5 h-screen w-screen fixed top-0 left-0 -z-10"></div> */}
                 <div
