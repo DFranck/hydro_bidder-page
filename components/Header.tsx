23352180db853c7f459e5f9b1e0fdebfb46c9d26
Aaron Wright <aaronmw@gmail.com>
2024-10-22 15:42:05 -0400

    Add banner re: pilot rounds

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,7 @@ import { ContentContainer } from "@/components/ContentContainer"
 import { useUserVotingData } from "@/hooks/hooks"
 import { useIsDocumentScrolled } from "@/lib/useIsDocumentScrolled"
 import { useChain } from "@cosmos-kit/react"
+import { ArrowUpRight } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 import { usePathname, useRouter } from "next/navigation"
@@ -16,7 +17,6 @@ export const Header = () => {
     const { address } = useChain("neutron")
     const previousAddress = useDeferredValue(address)
     const { data: userVotingData } = useUserVotingData(address || "")
-    const showLockATOMBanner = userVotingData?.votingPower === 0
     const pathname = usePathname()
     const router = useRouter()
 
@@ -98,40 +98,40 @@ export const Header = () => {
                 </div>
             </ContentContainer>
 
-            {showLockATOMBanner && (
-                <div
-                    className={twMerge(
+            <div
+                className={twMerge(
+                    `
+                        relative
+                        bg-palette-green
+                        px-24
+                        text-center
+                        text-palette-text
+                        transition-all
+                        duration-1000
+                    `,
+                    isScrolled
+                        ? `
+                            py-1.5
+                            text-xs
                         `
-                            relative
-                            bg-palette-green
-                            px-24
-                            text-center
-                            text-palette-text
-                            transition-all
-                            duration-1000
-                        `,
-                        isScrolled
-                            ? `
-                                py-1.5
-                                text-xs
-                            `
-                            : `
-                                py-2
-                                text-sm
-                            `
-                    )}
+                        : `
+                            py-2
+                            text-sm
+                        `
+                )}
+            >
+                Hydro is currently running pilot rounds.{" "}
+                <span className="inline-flex items-center gap-1 font-bold underline">
+                    Learn More <ArrowUpRight size={16} />
+                </span>
+                <Link
+                    className="absolute inset-0 z-10"
+                    href="/docs#pilot-rounds"
+                    target="_blank"
                 >
-                    Hydro is currently running pilot rounds.{" "}
-                    <span className="font-bold underline">Learn More</span>
-                    <Link
-                        className="absolute inset-0 z-10"
-                        href="/docs"
-                        target="_blank"
-                    >
-                        <span className="sr-only">Learn More</span>
-                    </Link>
-                </div>
-            )}
+                    <span className="sr-only">Learn More</span>
+                </Link>
+            </div>
         </div>
     )
 }
