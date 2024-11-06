8462e8dddc525d8e6eab7d26706a987e8edc84ad
Aaron Wright <aaronmw@gmail.com>
2024-11-06 15:27:06 -0500

    Fix header resize nonsense

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,13 +2,12 @@
 
 import { ContentContainer } from "@/components/ContentContainer"
 import { Icon } from "@/components/Icon"
-import { useUserVotingData } from "@/hooks/hooks"
 import { useIsDocumentScrolled } from "@/lib/useIsDocumentScrolled"
 import { useChain } from "@cosmos-kit/react"
 import Image from "next/image"
 import Link from "next/link"
 import { usePathname, useRouter } from "next/navigation"
-import { useDeferredValue, useEffect } from "react"
+import { useDeferredValue, useEffect, useRef } from "react"
 import { twMerge } from "tailwind-merge"
 import Navigation from "./Navigation"
 
@@ -16,9 +15,10 @@ export const Header = () => {
   const { isDocumentScrolled: isScrolled } = useIsDocumentScrolled()
   const { address } = useChain("neutron")
   const previousAddress = useDeferredValue(address)
-  const { data: userVotingData } = useUserVotingData(address || "")
   const pathname = usePathname()
   const router = useRouter()
+  const elementRef = useRef<HTMLDivElement>(null)
+  const ghostElementRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
     const hasRedirected = window.sessionStorage.getItem("redirected") === "true"
@@ -34,99 +34,111 @@ export const Header = () => {
     }
   }, [address, pathname, router, previousAddress])
 
+  useEffect(() => {
+    if (elementRef.current && ghostElementRef.current) {
+      ghostElementRef.current.style.height = `${elementRef.current.clientHeight}px`
+    }
+  }, [])
+
   return (
-    <div
-      className="
-        sticky
-        left-0
-        right-0
-        top-0
-        z-40
-        border-b
-        border-palette-beige
-      "
-    >
-      <ContentContainer
-        className={twMerge(
-          `
-            flex-row
-            items-center
-            justify-between
-            bg-black
-            text-sm
-            transition-all
-            duration-300
-          `,
-          isScrolled
-            ? `
-              py-1
-            `
-            : `
-              py-3
-            `
-        )}
+    <>
+      {/* Ghost element to maintain height despite shrinking on scroll */}
+      <div ref={ghostElementRef} className="pointer-events-none" />
+
+      <div
+        className="
+          fixed
+          left-0
+          right-0
+          top-0
+          z-40
+          border-b
+          border-palette-beige
+        "
+        ref={elementRef}
       >
-        <div
+        <ContentContainer
           className={twMerge(
             `
-              relative
+              flex-row
+              items-center
+              justify-between
+              bg-black
+              text-sm
               transition-all
               duration-300
             `,
             isScrolled
               ? `
-                  h-8
-                  w-40
-                `
+                py-1
+              `
               : `
-                  h-12
-                  w-56
-                `
+                py-3
+              `
           )}
         >
-          <Link href="/">
-            <Image src={"/images/logo.svg"} alt="twitter" fill={true} />
+          <div
+            className={twMerge(
+              `
+                relative
+                transition-all
+                duration-300
+              `,
+              isScrolled
+                ? `
+                    h-8
+                    w-40
+                  `
+                : `
+                    h-12
+                    w-56
+                  `
+            )}
+          >
+            <Link href="/">
+              <Image src={"/images/logo.svg"} alt="Hydro Logo" fill={true} />
+            </Link>
+          </div>
+          <div className="flex flex-row items-center justify-between gap-6">
+            <Navigation />
+          </div>
+        </ContentContainer>
+
+        <div
+          className={twMerge(
+            `
+              relative
+              bg-palette-green
+              px-24
+              text-center
+              text-palette-text
+              transition-all
+              duration-300
+            `,
+            isScrolled
+              ? `
+                py-1.5
+                text-xs
+              `
+              : `
+                py-2
+                text-sm
+              `
+          )}
+        >
+          Hydro is currently running its first pilot round.{" "}
+          <span className="inline-flex items-center gap-1 font-bold underline">
+            Learn More <Icon name="solid:arrow-up-right" />
+          </span>
+          <Link
+            className="absolute inset-0 z-10"
+            href="/docs#pilot-rounds"
+            target="_blank"
+          >
+            <span className="sr-only">Learn More</span>
           </Link>
         </div>
-        <div className="flex flex-row items-center justify-between gap-6">
-          <Navigation />
-        </div>
-      </ContentContainer>
-
-      <div
-        className={twMerge(
-          `
-            relative
-            bg-palette-green
-            px-24
-            text-center
-            text-palette-text
-            transition-all
-            duration-300
-          `,
-          isScrolled
-            ? `
-              py-1.5
-              text-xs
-            `
-            : `
-              py-2
-              text-sm
-            `
-        )}
-      >
-        Hydro is currently running its first pilot round.{" "}
-        <span className="inline-flex items-center gap-1 font-bold underline">
-          Learn More <Icon name="solid:arrow-up-right" />
-        </span>
-        <Link
-          className="absolute inset-0 z-10"
-          href="/docs#pilot-rounds"
-          target="_blank"
-        >
-          <span className="sr-only">Learn More</span>
-        </Link>
       </div>
-    </div>
+    </>
   )
 }
