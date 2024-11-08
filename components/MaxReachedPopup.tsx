5495d262148912643cc4d119952f6a4e649ab1b7
Aaron Wright <aaronmw@gmail.com>
2024-11-08 17:06:16 -0500

    Add state to max reached modal

---
+++ components/MaxReachedPopup.tsx
@@ -0,0 +1,100 @@
+import { Card } from "@/components/Card"
+import { Icon } from "@/components/Icon"
+import { ModalWindow } from "@/components/ModalWindow"
+import { StyledText } from "@/components/StyledText"
+import Link from "next/link"
+import { ChangeEvent, useEffect, useState } from "react"
+import { useLocalStorage } from "usehooks-ts"
+
+export function MaxReachedPopup() {
+  const [dontShowAgain, setDontShowAgain] = useLocalStorage(
+    "dont-show-max-reached-popup-again",
+    false
+  )
+  const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    if (dontShowAgain) {
+      return
+    }
+
+    setIsOpen(true)
+  }, [dontShowAgain])
+
+  function handleClose() {
+    setIsOpen(false)
+  }
+
+  return (
+    <ModalWindow isOpen={isOpen} onClose={handleClose}>
+      <Card>
+        <Card.Header>Pilot Round 1 Lock Cap Reached</Card.Header>
+        <Card.Body>
+          <div className="prose prose-invert">
+            <p>
+              The maximum amount of ATOM that can be locked in this round has
+              been reached. But things are far from over!
+            </p>
+            <p>
+              Keep optimizing your votes to ensure they go to the most rewarding
+              projects. Remember, projects may increase their tribute to attract
+              more votes before the round ends, so check back often.
+            </p>
+            <p>Don&rsquo;t miss out on the next phase:</p>
+            <ul>
+              <li>
+                <strong>Have a lockup?</strong> Continue optimizing your
+                strategy and join our{" "}
+                <StyledText
+                  variant="link"
+                  as={Link}
+                  href="https://t.me/+xUzNOTZjUNw5Mzhk"
+                  target="_blank"
+                >
+                  Telegram Group
+                  <Icon name="solid:arrow-up-right" />
+                </StyledText>{" "}
+                to stay updated.
+              </li>
+              <li>
+                <strong>No lockup yet?</strong> Join the{" "}
+                <StyledText
+                  variant="link"
+                  as={Link}
+                  href="https://t.me/+xUzNOTZjUNw5Mzhk"
+                  target="_blank"
+                >
+                  Telegram Group
+                  <Icon name="solid:arrow-up-right" />
+                </StyledText>{" "}
+                to be the first to know when Pilot Round 2 kicks off!
+              </li>
+            </ul>
+          </div>
+        </Card.Body>
+        <Card.Footer className="justify-between">
+          <StyledText
+            variant="button.primary"
+            as="button"
+            onClick={handleClose}
+          >
+            Close
+          </StyledText>
+
+          <label className="flex items-center gap-2">
+            <StyledText
+              as="input"
+              type="checkbox"
+              variant="input.checkbox"
+              checked={dontShowAgain}
+              onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                setDontShowAgain(event.target.checked)
+              }
+            />
+            <StyledText variant="label">Don&rsquo;t show this again</StyledText>
+          </label>
+        </Card.Footer>
+      </Card>
+    </ModalWindow>
+  )
+}
