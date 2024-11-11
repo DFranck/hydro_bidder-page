b27ceb3c1888dfe3ebc947774d90b0eb261efee0
Aaron Wright <aaronmw@gmail.com>
2024-11-11 10:04:46 -0500

    Mark hardcoded stuff

--- app/middleware.ts
+++ app/middleware.ts
@@ -2,6 +2,7 @@ import type { NextRequest } from "next/server"
 import { NextResponse } from "next/server"
 
 export function middleware(request: NextRequest) {
+  // TODO: remove this hardcoded redirect
   if (request.nextUrl.pathname === "/lock-atom") {
     return NextResponse.redirect(new URL("/voting", request.url))
   }
