96249f95b4f40c41b9bd90e40f9ecb27f4ae15bf
Aaron Wright <aaronmw@gmail.com>
2024-11-18 16:03:39 -0500

    Fix metrics crash

--- hooks/hooks.ts
+++ hooks/hooks.ts
@@ -68,6 +68,39 @@ export type ValidatorDelegation = {
   }
 }
 
+export interface ProposalFromNumia {
+  round: string
+  tranche: string
+  project: string
+  project_url: string
+  project_logo_url: string
+  id: string
+  title: string
+  description: string
+  comments: string
+  onchain_tribute_assets: string
+  onchain_tribute_usdc: number
+  offchain_tribute: string
+  voters: number
+  voting_power: number
+  requested_allocation_denom: string
+  requested_allocation_amount: number
+  initial_allocation_denom: string
+  initial_allocation_amount: number
+  current_allocation_denom: string
+  current_allocation_amount: number
+  status: string
+  duration_days: number
+  apr: number
+}
+
+export async function fetchNumiaData(): Promise<ProposalFromNumia[]> {
+  const response = await fetch(
+    "https://www.datalenses.zone/numia/cosmos/lensesV2/hydro/deployments_overview"
+  )
+  return response.json()
+}
+
 export async function fetchDashboardData() {
   const globalState = await fetchGlobalState()
 
