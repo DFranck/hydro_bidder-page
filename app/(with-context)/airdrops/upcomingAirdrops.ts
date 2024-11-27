3e1816797b673fda6f2abb49498920d0c5a4d961
Brian Truax <Briantruax@gmail.com>
2024-11-27 15:11:03 -0600

    Update upcomingAirdrops.ts

--- app/(with-context)/airdrops/upcomingAirdrops.ts
+++ app/(with-context)/airdrops/upcomingAirdrops.ts
@@ -1,4 +1,23 @@
 export const upcomingAirdrops = `Elys	Elys Network makes crypto simple with universal access to tokens from all ecosystems. Whether you’re a novice or an experienced user, you can easily navigate the DeFi space through a complete set of features, all in one place.	Confirmed	Locked ATOM in Round 1	Check eligibility	Text	Waiting for link		FALSE
 Side Protocol	Side Protocol is a fully Bitcoin-compatible Layer 1 blockchain	Confirmed	 Locked > 15 ATOM in Round 1	Register for the airdrop	Button	Register Now	https://genesis.side.one/	FALSE
 Dungeon Chain	Dungeon Chain (DGN) revolutionizes gaming with fast, low-cost blockchain tech secured by Cosmos Hub validators; first game, Kosmic Quest, launches Q1 2025.	Confirmed	Locked ATOM in Round 1	No action required	Text	Dropped to your wallet		FALSE
-Drop	Drop, a liquid staking protocol backed by Lido, turns idle assets into economic flows to empower sovereign blockchains.	Confirmed	Details Pending	Details Pending	Text	Waiting for link		FALSE`
+Drop	Drop, a liquid staking protocol backed by Lido, turns idle assets into economic flows to empower sovereign blockchains.	Confirmed	Details Pending	Details Pending	Text	Details Pending		FALSE
+								
+Namada	Asset-agnostic privacy Layer 1 blockchain enabling shielded transfers with zero-knowledge proofs.	Rumored	Details Pending	Details Pending	Text	Details Pending		FALSE
+Soarchain	A DePIN project that incorporates AI and modularity, focusing on the mobility ecosystem.	Rumored	Details Pending	Details Pending	Text	Details Pending		FALSE
+Electron	The first DeFi Protocol designed to integrate RWAs on Neutron.	Rumored	Details Pending	Details Pending	Text	Details Pending		FALSE
+TFM	TFM is the "Modular Liquidity Layer" for 100+ Cosmos Chains with 4 pro trading products live.	Rumored	Details Pending	Details Pending	Text	Details Pending		FALSE
+Eris Protocol	Liquid staking and slow burn arbitrage protocol built on Terra and Terra Classic.	Rumored	Details Pending	Details Pending	Text	Details Pending		FALSE
+Initia	Initia is a network for interwoven rollups. Binding a highly interwoven system of modular networks through architectural ownership.	Rumored	Details Pending	Details Pending	Text	Details Pending		FALSE
+Backbone	A multi-chain NFTfi Hub for the Cosmos, with DeFi products managed by NFT communities.	Rumored	Details Pending	Details Pending	Text	Details Pending		FALSE
+Prysm	ICS Hub providing foundational infrastructure, enhanced connectivity, security, and scalability.	Rumored	Details Pending	Details Pending	Text	Details Pending		FALSE
+Arkeo	Decentralized and indexed blockchain data for any chain.	Rumored	Details Pending	Details Pending	Text	Details Pending		FALSE
+AdamantFi	"Privacy-preserving liquidity protocol powered by Secret Network
+ and $ADMT"	Rumored	Details Pending	Details Pending	Text	Details Pending		FALSE
+Nomic	A Proof-of-Stake network running alongside Bitcoin to securely bring native BTC to other networks.	Rumored	Details Pending	Details Pending	Text	Details Pending		FALSE
+Flip	Flip offers a dedicated DeFi playground tailored specifically for Degens, promising to revolutionize the trading experience.	Rumored	Details Pending	Details Pending	Text	Details Pending		FALSE
+Toki	TOKI is a cross-chain bridge that employs multi-prover security, leveraging TEEs and ZKP for IBC.	Rumored	Details Pending	Details Pending	Text	Details Pending		FALSE
+Movement	A next-generation blockchain network, claiming to power the fastest and most secure layer two on ethereum	Rumored	Details Pending	Details Pending	Text	Details Pending		FALSE
+Nillion	Nillion decentralizes trust for sensitive data in the same way that blockchains decentralized transactions.	Rumored	Details Pending	Details Pending	Text	Details Pending		FALSE
+Berachain	Berachain is a high-performance EVM-compatible blockchain built on Proof-of-Liquidity consensus.	Rumored	Details Pending	Details Pending	Text	Details Pending		FALSE
+Hyperlane	Hyperlane is an open interoperability framework for blockchain smart contract devs and users. Send and receive messages, bridge ERC-20 tokens & NFTs and mroe.	Rumored	Details Pending	Details Pending	Text	Details Pending		FALSE`
