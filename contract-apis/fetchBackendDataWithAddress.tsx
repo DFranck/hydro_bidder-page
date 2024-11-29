ab3faae96500f209d30470e29abd71e816adfb72
Aaron Wright <aaronmw@gmail.com>
2024-11-29 16:37:57 -0500

    use server sparingly

--- contract-apis/fetchBackendDataWithAddress.tsx
+++ contract-apis/fetchBackendDataWithAddress.tsx
@@ -13,6 +13,7 @@ import {
   keysFromSnakeToCamelCase,
 } from "@/lib/keysFromSnakeToCamelCase"
 import { sumBy } from "lodash"
+import { connection } from "next/server"
 
 export interface BackendDataWithAddress
   extends Omit<BackendData, "bidsByRoundId"> {
@@ -44,6 +45,8 @@ export async function fetchBackendDataWithAddress({
   address: string
   backendData: BackendData
 }): Promise<BackendDataWithAddress> {
+  await connection()
+
   if (!process.env.NEXT_PUBLIC_HYDRO_CONTRACT_ADDRESS) {
     throw new Error("Hydro contract address not set")
   }
