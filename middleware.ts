d4db483a76910f44559cbc61409d28555c82b1fa
Aaron Wright <aaronmw@gmail.com>
2024-11-29 15:16:58 -0500

    Remove needless header

--- middleware.ts
+++ middleware.ts
@@ -9,34 +9,25 @@ export function middleware(request: NextRequest) {
     const response = NextResponse.redirect(
       new URL(pathname.replace("/voting", "/bids"), request.url)
     )
-    response.headers.set("x-pathname", pathname)
     return response
   }
 
   // Redirects below only work in production environments
   if (process.env.CONTEXT !== "production") {
-    const response = NextResponse.next()
-    response.headers.set("x-pathname", pathname)
-    return response
+    return NextResponse.next()
   }
 
   // TODO: remove this hardcoded redirect
   // redirecting /airdrops -> /
   if (pathname.startsWith("/airdrops")) {
-    const response = NextResponse.redirect(new URL("/", request.url))
-    response.headers.set("x-pathname", pathname)
-    return response
+    return NextResponse.redirect(new URL("/", request.url))
   }
 
   // TODO: remove this hardcoded redirect
   // redirecting /lock-atom -> /bids
   if (pathname.startsWith("/lock-atom")) {
-    const response = NextResponse.redirect(new URL("/bids", request.url))
-    response.headers.set("x-pathname", pathname)
-    return response
+    return NextResponse.redirect(new URL("/bids", request.url))
   }
 
-  const response = NextResponse.next()
-  response.headers.set("x-pathname", pathname)
-  return response
+  return NextResponse.next()
 }
