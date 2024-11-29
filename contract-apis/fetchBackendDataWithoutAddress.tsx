11a2524741c1720dddadb97ffee29356e90fe4a6
Aaron Wright <aaronmw@gmail.com>
2024-11-29 16:01:47 -0500

    Dont await connection now?

--- contract-apis/fetchBackendDataWithoutAddress.tsx
+++ contract-apis/fetchBackendDataWithoutAddress.tsx
@@ -22,7 +22,6 @@ import {
   CamelCaseKeys,
   keysFromSnakeToCamelCase,
 } from "@/lib/keysFromSnakeToCamelCase"
-import { connection } from "next/server"
 
 export interface BidFromContract extends Proposal {}
 
@@ -85,8 +84,6 @@ export const initialBackendData: BackendData = {
 }
 
 export async function fetchBackendDataWithoutAddress(): Promise<BackendData> {
-  await connection()
-
   if (!process.env.NEXT_PUBLIC_HYDRO_CONTRACT_ADDRESS) {
     throw new Error("Hydro contract address not set")
   }
