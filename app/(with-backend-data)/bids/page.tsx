b82d16e324b31b54f8b2ef3469fcce899ecffb40
Aaron Wright <aaronmw@gmail.com>
2024-12-11 14:35:58 -0500

    Move bid details below

--- app/(with-backend-data)/bids/page.tsx
+++ app/(with-backend-data)/bids/page.tsx
@@ -137,7 +137,7 @@ export default function BidsPage() {
         ),
         actions: (
           <InvisibleLink href={bidURL}>
-            <div className="flex items-center justify-end gap-3">
+            <div className="flex flex-col items-center justify-end gap-2">
               <VoteButton bidId={bid.id} size="small" />
               <StyledText variant="link" className={classNames.bidDetailsLink}>
                 <span>Bid Details</span> <Icon name="chevron-right" />
