a891daba7ff4e3c44694ba27023e00387e227d52
Brian Truax <Briantruax@gmail.com>
2024-12-12 14:50:07 -0600

    Update upcomingAirdrops.ts

--- app/(with-backend-data)/airdrops/upcomingAirdrops.ts
+++ app/(with-backend-data)/airdrops/upcomingAirdrops.ts
@@ -1,26 +1,26 @@
-export const upcomingAirdrops = `Elys	Elys Network makes crypto simple with universal access to tokens from all ecosystems. Whether you’re a novice or an experienced user, you can easily navigate the DeFi space through a complete set of features, all in one place.	Confirmed	Text	Details Pending		FALSE
+export const upcomingAirdrops = `Elys	Elys Network makes crypto simple with universal access to tokens from all ecosystems. Whether you’re a novice or an experienced user, you can easily navigate the DeFi space through a complete set of features, all in one place.	Confirmed	Text	Pending		FALSE
 Side Protocol	Side Protocol is a fully Bitcoin-compatible Layer 1 blockchain	Confirmed	Button	More Info	https://genesis.side.one/	FALSE
-Dungeon Chain	Dungeon Chain (DGN) revolutionizes gaming with fast, low-cost blockchain tech secured by Cosmos Hub validators; first game, Kosmic Quest, launches Q1 2025.	Confirmed	Text	Details Pending		FALSE
-Drop	Drop, a liquid staking protocol backed by Lido, turns idle assets into economic flows to empower sovereign blockchains.	Confirmed	Text	Details Pending		FALSE
-Demex	Demex is a seamless, omni-chain DEX for trading perpetuals, spot, and earning yield across chains.	Confirmed	Text	Details Pending		FALSE
-Namada	Asset-agnostic privacy Layer 1 blockchain enabling shielded transfers with zero-knowledge proofs.	Rumored	Text	Details Pending		FALSE
-Soarchain	A DePIN project that incorporates AI and modularity, focusing on the mobility ecosystem.	Rumored	Text	Details Pending		FALSE
-Electron	The first DeFi Protocol designed to integrate RWAs on Neutron.	Rumored	Text	Details Pending		FALSE
-TFM	TFM is the "Modular Liquidity Layer" for 100+ Cosmos Chains with 4 pro trading products live.	Rumored	Text	Details Pending		FALSE
-Eris Protocol	Liquid staking and slow burn arbitrage protocol built on Terra and Terra Classic.	Rumored	Text	Details Pending		FALSE
-Initia	Initia is a network for interwoven rollups. Binding a highly interwoven system of modular networks through architectural ownership.	Rumored	Text	Details Pending		FALSE
-Backbone	A multi-chain NFTfi Hub for the Cosmos, with DeFi products managed by NFT communities.	Rumored	Text	Details Pending		FALSE
-Prysm	ICS Hub providing foundational infrastructure, enhanced connectivity, security, and scalability.	Rumored	Text	Details Pending		FALSE
-Arkeo	Decentralized and indexed blockchain data for any chain.	Rumored	Text	Details Pending		FALSE
-AdamantFi	Privacy-preserving liquidity protocol powered by Secret Network and ADMT	Rumored	Text	Details Pending		FALSE
-Nomic	A Proof-of-Stake network running alongside Bitcoin to securely bring native BTC to other networks.	Rumored	Text	Details Pending		FALSE
-Flip	Flip offers a dedicated DeFi playground tailored specifically for Degens, promising to revolutionize the trading experience.	Rumored	Text	Details Pending		FALSE
-Toki	TOKI is a cross-chain bridge that employs multi-prover security, leveraging TEEs and ZKP for IBC.	Rumored	Text	Details Pending		FALSE
-Movement	A next-generation blockchain network, claiming to power the fastest and most secure layer two on ethereum	Rumored	Text	Details Pending		FALSE
-Nillion	Nillion decentralizes trust for sensitive data in the same way that blockchains decentralized transactions.	Rumored	Text	Details Pending		FALSE
-Berachain	Berachain is a high-performance EVM-compatible blockchain built on Proof-of-Liquidity consensus.	Rumored	Text	Details Pending		FALSE
-Hyperlane	Hyperlane is an open interoperability framework for blockchain smart contract devs and users. Send and receive messages, bridge ERC-20 tokens & NFTs and mroe.	Rumored	Text	Details Pending		FALSE
-Babylon	Unlocking 21 Milion Bitcoins to Secure the Decentralized Economy	Rumored	Text	Details Pending		FALSE
-Symphony	The first self-balancing stablecoin blockchain	Rumored	Text	Details Pending		FALSE
-Tabi	Gaming L1 with any VM-compatibility	Rumored	Text	Details Pending		FALSE
-Aaron Network	Independent messaging exchange platform	Rumored	Text	Details Pending		FALSE`
+Dungeon Chain	Dungeon Chain (DGN) revolutionizes gaming with fast, low-cost blockchain tech secured by Cosmos Hub validators; first game, Kosmic Quest, launches Q1 2025.	Confirmed	Text	Pending		FALSE
+Drop	Drop, a liquid staking protocol backed by Lido, turns idle assets into economic flows to empower sovereign blockchains.	Confirmed	Text	Pending		FALSE
+Demex	Demex is a seamless, omni-chain DEX for trading perpetuals, spot, and earning yield across chains.	Confirmed	Text	Pending		FALSE
+Namada	Asset-agnostic privacy Layer 1 blockchain enabling shielded transfers with zero-knowledge proofs.	Rumored	Text	Pending		FALSE
+Soarchain	A DePIN project that incorporates AI and modularity, focusing on the mobility ecosystem.	Rumored	Text	Pending		FALSE
+Electron	The first DeFi Protocol designed to integrate RWAs on Neutron.	Rumored	Text	Pending		FALSE
+TFM	TFM is the "Modular Liquidity Layer" for 100+ Cosmos Chains with 4 pro trading products live.	Rumored	Text	Pending		FALSE
+Eris Protocol	Liquid staking and slow burn arbitrage protocol built on Terra and Terra Classic.	Rumored	Text	Pending		FALSE
+Initia	Initia is a network for interwoven rollups. Binding a highly interwoven system of modular networks through architectural ownership.	Rumored	Text	Pending		FALSE
+Backbone	A multi-chain NFTfi Hub for the Cosmos, with DeFi products managed by NFT communities.	Rumored	Text	Pending		FALSE
+Prysm	ICS Hub providing foundational infrastructure, enhanced connectivity, security, and scalability.	Rumored	Text	Pending		FALSE
+Arkeo	Decentralized and indexed blockchain data for any chain.	Rumored	Text	Pending		FALSE
+AdamantFi	Privacy-preserving liquidity protocol powered by Secret Network and ADMT	Rumored	Text	Pending		FALSE
+Nomic	A Proof-of-Stake network running alongside Bitcoin to securely bring native BTC to other networks.	Rumored	Text	Pending		FALSE
+Flip	Flip offers a dedicated DeFi playground tailored specifically for Degens, promising to revolutionize the trading experience.	Rumored	Text	Pending		FALSE
+Toki	TOKI is a cross-chain bridge that employs multi-prover security, leveraging TEEs and ZKP for IBC.	Rumored	Text	Pending		FALSE
+Movement	A next-generation blockchain network, claiming to power the fastest and most secure layer two on ethereum	Rumored	Text	Pending		FALSE
+Nillion	Nillion decentralizes trust for sensitive data in the same way that blockchains decentralized transactions.	Rumored	Text	Pending		FALSE
+Berachain	Berachain is a high-performance EVM-compatible blockchain built on Proof-of-Liquidity consensus.	Rumored	Text	Pending		FALSE
+Hyperlane	Hyperlane is an open interoperability framework for blockchain smart contract devs and users. Send and receive messages, bridge ERC-20 tokens & NFTs and mroe.	Rumored	Text	Pending		FALSE
+Babylon	Unlocking 21 Milion Bitcoins to Secure the Decentralized Economy	Rumored	Text	Pending		FALSE
+Symphony	The first self-balancing stablecoin blockchain	Rumored	Text	Pending		FALSE
+Tabi	Gaming L1 with any VM-compatibility	Rumored	Text	Pending		FALSE
+Aaron Network	Independent messaging exchange platform	Rumored	Text	Pending		FALSE`
