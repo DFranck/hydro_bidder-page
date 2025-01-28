30d8a6e58251b30b05f9ea6bfb66b37f39525591
Aaron Wright <aaronmw@gmail.com>
2025-01-28 17:40:16 -0500

    Improve number rendering; update metrics page cols

--- components/BidTributes.tsx
+++ components/BidTributes.tsx
@@ -1,25 +1,18 @@
+import { AmountAndUnitPair } from "@/components/AmountAndUnitPair"
 import { Icon } from "@/components/Icon"
-import { StyledText } from "@/components/StyledText"
 import { Tooltip } from "@/components/Tooltip"
 import { bidTypeTooltip } from "@/components/ToolTips"
 import { AugmentedBid } from "@/contract-apis/fetchBackendDataAfterWallet"
 import { useBackendData } from "@/contract-apis/useBackendData"
 import { simplifyBigNumbers } from "@/lib/simplifyBigNumbers"
 import { groupBy } from "lodash"
-import Link from "next/link"
 import { twJoin } from "tailwind-merge"
 
 export function BidTributes({
   bid,
-  classNamesForValues,
-  classNamesForDenoms,
-  denomsOnly = false,
   textAlign = "left",
 }: {
   bid: AugmentedBid
-  classNamesForValues?: string
-  classNamesForDenoms?: string
-  denomsOnly?: boolean
   textAlign?: "left" | "center" | "right"
 }) {
   const { bidDescriptionsByBidId, metricsForPostHydroBids } = useBackendData()
@@ -31,8 +24,7 @@ export function BidTributes({
     return 0
   }
 
-  const { onchainTributeAssets, onchainTributeUsdc, offchainTribute } =
-    bidInfoFromNumia
+  const { onchainTributeAssets, offchainTribute } = bidInfoFromNumia
 
   const tributesByDenom = {
     ...groupBy(
@@ -47,52 +39,52 @@ export function BidTributes({
 
   const bidDescription = bidDescriptionsByBidId[bid.id]
 
+  const { pointProgramUrl } = bidDescription ?? {}
+
   const renderedTributes = Object.entries(tributesByDenom).map(
-    ([denomOrType, tributes]) => {
-      return tributes.map((tribute, index) => {
-        const isTokenBased = tribute.tributeType === "tokens"
-        const isPointBased = tribute.tributeType === "points"
+    ([denomOrType, tributes], index) => {
+      return (
+        <div
+          className={twJoin(
+            "flex flex-col",
+            textAlign === "left" && "items-start",
+            textAlign === "center" && "items-center",
+            textAlign === "right" && "items-end"
+          )}
+          key={index}
+        >
+          {tributes.map((tribute, index) => {
+            const isTokenBased = tribute.tributeType === "tokens"
+            const isPointBased = tribute.tributeType === "points"
 
-        return (
-          <Tooltip
-            key={index}
-            tipContents={bidTypeTooltip({ isTokenBased })}
-            className={twJoin(
-              "inline-flex flex-col",
-              textAlign === "left" && "items-start",
-              textAlign === "center" && "items-center",
-              textAlign === "right" && "items-end"
-            )}
-          >
-            <div
-              className={twJoin(
-                "inline-flex items-center gap-1",
-                textAlign === "left" && "justify-start",
-                textAlign === "center" && "justify-center",
-                textAlign === "right" && "justify-end"
-              )}
-            >
-              {isPointBased && <Icon name="solid:gem" />}
-              {simplifyBigNumbers(tribute.amount)}{" "}
-              <span className="opacity-60">{denomOrType}</span>
-            </div>
-
-            {isPointBased &&
-              bidDescription &&
-              bidDescription.pointProgramUrl && (
-                <StyledText
-                  as={Link}
-                  href={bidDescription.pointProgramUrl}
-                  variant="link"
-                  className="inline-flex items-center gap-1 text-xs"
-                >
-                  <span>Learn More</span>
-                  <Icon name="arrow-up-right-from-square" />
-                </StyledText>
-              )}
-          </Tooltip>
-        )
-      })
+            return (
+              <Tooltip
+                key={index}
+                tipContents={bidTypeTooltip({
+                  isTokenBased,
+                  pointProgramUrl,
+                })}
+              >
+                <AmountAndUnitPair
+                  amount={
+                    <>
+                      {isPointBased && <Icon name="solid:gem" />}{" "}
+                      {simplifyBigNumbers(tribute.amount)}
+                    </>
+                  }
+                  unit={
+                    <>
+                      {denomOrType}
+                      {isPointBased && <Icon name="circle-info" />}
+                    </>
+                  }
+                  textAlign={textAlign}
+                />
+              </Tooltip>
+            )
+          })}
+        </div>
+      )
     }
   )
 
