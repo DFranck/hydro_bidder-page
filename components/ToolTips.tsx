dcfb8cc3a29d33f33715b9aeb7cc56a500a84b1d
Aaron Wright <aaronmw@gmail.com>
2025-02-05 16:04:10 -0500

    Fix conflict

--- components/ToolTips.tsx
+++ components/ToolTips.tsx
@@ -17,7 +17,7 @@ export const averageAPRTooltip = (
     <p>
       The average APR available to Hydro voters during the current active round.
       Hydro APR is separate and additional to your staking APR as an ATOM
-      staker.
+      staker.{" "}
       <StyledText
         variant="link"
         as={Link}
@@ -49,73 +49,66 @@ export const averageRoundsPerUserTooltip = (
 export const bidTablesFirstColumnTooltips = {
   bidsTable: {
     tokenBased: (
-      <div className="flex flex-col gap-2">
-        <p>These bids uses a token as their tribute to incentivize Hydro voters to vote for them.</p>
-      </div>
+      <p>
+        These bids use a token as their tribute to incentivize Hydro voters to
+        vote for them.
+      </p>
     ),
     pointBased: (
-      <div className="flex flex-col gap-2">
-        <p>These bids uses a point system as their tribute to incentivize Hydro voters to vote for them.</p>
-      </div>
+      <p>
+        These bids use a point system as their tribute to incentivize Hydro
+        voters to vote for them.
+      </p>
     ),
   },
   metricsTable: {
-    tokenBased: (
-      <div className="flex flex-col gap-2">
-        <p>These bids used a live token as tribute for their bid.</p>
-      </div>
-    ),
+    tokenBased: <p>These bids used a live token as tribute for their bid.</p>,
     pointBased: (
-      <div className="flex flex-col gap-2">
-        <p>These bids used a points system as tribute for their bid.</p>
-      </div>
+      <p>These bids used a points system as tribute for their bid.</p>
     ),
   },
 }
 
 export const tokenBasedTributeAmountTooltip = (
-  <>
-    <p>This bid used a token as their tribute to incentivize Hydro voters to vote for them.</p>
-  </>
+  <p>
+    This bid used a token as their tribute to incentivize Hydro voters to vote
+    for them.
+  </p>
 )
 
 export const pointBasedTributeAmountTooltip = ({
   pointProgramUrl,
 }: {
   pointProgramUrl?: string
-}) => (
-  <>
-
-    {pointProgramUrl && (
-      <p>
-        The amount offered as tribute by this bid to incentivize Hydro voters to
-        allocate liquidity to it. This bid uses points as its tribute because it
-        does not yet have a live token.{" "}
-        <StyledText
-          as={Link}
-          href={pointProgramUrl}
-          variant="link"
-          className="inline-flex items-center gap-1"
-        >
-          Learn More <Icon name="solid:arrow-up-right" />
-        </StyledText>
-      </p>
-    )}
-  </>
-)
+}) =>
+  pointProgramUrl && (
+    <p>
+      The amount offered as tribute by this bid to incentivize Hydro voters to
+      allocate liquidity to it. This bid uses points as its tribute because it
+      does not yet have a live token.{" "}
+      <StyledText
+        as={Link}
+        href={pointProgramUrl}
+        variant="link"
+        className="inline-flex items-center gap-1"
+      >
+        Learn More <Icon name="solid:arrow-up-right" />
+      </StyledText>
+    </p>
+  )
 
 export const currentVoteShareTooltip = (
-  <>
+  <p>
     The percentage of votes that this bid has received so far. It may change if
     other users decide to switch their vote before the round ends.
-  </>
+  </p>
 )
 
 export const cannotContinueLockupTooltip = (
-  <>
+  <p>
     This lockup is larger than the remaining capacity. You may either revert it
     to get back your staked ATOM, or wait and continue when there is capacity.
-  </>
+  </p>
 )
 
 export const estimatedRewardsColumnTooltip = ({
@@ -126,27 +119,27 @@ export const estimatedRewardsColumnTooltip = ({
   isTokenBased: boolean
 }) => {
   const universalPointSystemMessage = (
-    <>
+    <p>
       The total points offered by this bid as tribute to users. The tribute is
       split by the users that vote for this bid, based on their individual
       voting power.
-    </>
+    </p>
   )
   const messageIfHasVotedThisRound = isTokenBased ? (
-    <>
+    <p>
       The expected USD-equivalent value of tribute you would receive from the
       bid&rsquo;s tribute. Over time, the value may increase if the bidder adds
       tributes or decrease if more voters choose the bid.
-    </>
+    </p>
   ) : (
     universalPointSystemMessage
   )
   const messageIfHasNotVotedThisRound = isTokenBased ? (
-    <>
+    <p>
       The total estimated USD-equivalent value of the tribute offered in this
       bid. The tribute is split amongst the users that vote for the bid, based
       on their individual voting power.
-    </>
+    </p>
   ) : (
     universalPointSystemMessage
   )
@@ -193,21 +186,21 @@ export const estimatedRewardsTooltip = ({
     : "amount of points"
 
   const messageIfHasVotedThisRound = (
-    <>
+    <p>
       The {rewardDescription} you would receive from{" "}
       <strong>{projectName}</strong>. It represents{" "}
       <strong>{percentageOfTotalTributeValue}%</strong> of the total tribute{" "}
       <strong className="text-palette-beige">{formattedTotalTribute}</strong>.
       Over time, the value may increase if the bidder adds tribute or decrease
       if more voters choose the bid.
-    </>
+    </p>
   )
   const messageIfHasNotVotedThisRound = (
-    <>
+    <p>
       The {rewardDescription} offered by <strong>{projectName}</strong> as
       tribute. If you vote for this bid and it passes minimum thresholds, you
       will receive a portion of this value relative to your voting power.
-    </>
+    </p>
   )
 
   return hasVotedThisRound
@@ -216,11 +209,11 @@ export const estimatedRewardsTooltip = ({
 }
 
 export const lockupLimitReachedByUserTooltip = (
-  <>You&rsquo;ve reached the maximum locked tokens for this round.</>
+  <p>You&rsquo;ve reached the maximum locked tokens for this round.</p>
 )
 
 export const lockupLimitTooltip = (
-  <>
+  <p>
     During Pilot Rounds, there is a maximum limit of ATOM you can lockup.{" "}
     <StyledText
       as={Link}
@@ -232,11 +225,11 @@ export const lockupLimitTooltip = (
       Learn More
       <Icon name="solid:arrow-up-right" />
     </StyledText>
-  </>
+  </p>
 )
 
 export const lockupsTableVotingAndMultiplierColumnTooltip = (
-  <>
+  <p>
     Each lockup provides different voting power. The voting power and multiplier
     are determined by the amount of ATOM and duration of each lockup.{" "}
     <StyledText
@@ -249,19 +242,19 @@ export const lockupsTableVotingAndMultiplierColumnTooltip = (
       Learn more
       <Icon name="solid:arrow-up-right" />
     </StyledText>
-  </>
+  </p>
 )
 
 export const lockupsTableTimeLeftColumnTooltip = (
-  <>
+  <p>
     Voting power decays over time as the lockup period gets closer to
     expiration. You can edit a lockup at any time to extend the time left,
     reclaiming the max voting power for each lockup.
-  </>
+  </p>
 )
 
 export const lockAtomToVoteTooltip = (
-  <>
+  <p>
     All of your lockups are in use or expired. Lock more ATOM to vote for this
     bid.{" "}
     <StyledText
@@ -274,11 +267,11 @@ export const lockAtomToVoteTooltip = (
       <span>Learn More</span>
       <Icon name="arrow-up-right-from-square" />
     </StyledText>
-  </>
+  </p>
 )
 
 export const extendLockupsToVoteTooltip = (
-  <>
+  <p>
     You can extend your lockups to vote for this bid.{" "}
     <StyledText
       as={Link}
@@ -290,34 +283,34 @@ export const extendLockupsToVoteTooltip = (
       <span>Learn More</span>
       <Icon name="arrow-up-right-from-square" />
     </StyledText>
-  </>
+  </p>
 )
 
 export const longerLockupsComingSoonTooltip = (
-  <>Longer durations will be available after the pilot rounds.</>
+  <p>Longer durations will be available after the pilot rounds.</p>
 )
 
 export const metricsPolAprColumnTooltip = (
-  <>
+  <p>
     The Annual Percentage Rate (APR) representing the yield generated by this
     PoL deployment over its duration.
-  </>
+  </p>
 )
 
 export const metricsDurationColumnTooltip = (
-  <>The duration for which this PoL has been (or was) deployed.</>
+  <p>The duration for which this PoL has been (or was) deployed.</p>
 )
 
 export const metricsPolRewardsColumnTooltip = (
-  <>
+  <p>
     The rewards generated from this PoL deployment, including returns on
     liquidity and any additional benefits distributed to the Cosmos Hub
     Community Pool.
-  </>
+  </p>
 )
 
 export const metricsPolSizeColumnTooltip = (
-  <>
+  <p>
     The total amount of ATOM allocated to this bid as PoL during the specified
     round.{" "}
     <StyledText
@@ -330,20 +323,20 @@ export const metricsPolSizeColumnTooltip = (
       What is PoL?
       <Icon name="solid:arrow-up-right" />
     </StyledText>
-  </>
+  </p>
 )
 
 export const metricsStatusColumnTooltip = (
-  <>
+  <p>
     The current status of the liquidity deployment, such as &lsquo;Voting
     Period&rsquo;, &lsquo;Ongoing&rsquo; for active deployments,
     &lsquo;Completed&rsquo;, or &lsquo;Rejected&rsquo; for bids that did not
     meet minimum thresholds.
-  </>
+  </p>
 )
 
 export const metricsTributeColumnTooltip = (
-  <>
+  <p>
     Additional yield from tribute, rewarded by the bidder to voters who
     supported this bid in a Hydro auction. Tribute was not available Pre-Hydro.{" "}
     <StyledText
@@ -356,11 +349,11 @@ export const metricsTributeColumnTooltip = (
       Learn more
       <Icon name="solid:arrow-up-right" />
     </StyledText>
-  </>
+  </p>
 )
 
 export const metricsTributeAprColumnTooltip = (
-  <>
+  <p>
     Additional yield from tribute, rewarded by the bidder to voters who
     supported this bid in a Hydro auction.{" "}
     <StyledText
@@ -373,15 +366,15 @@ export const metricsTributeAprColumnTooltip = (
       Learn more
       <Icon name="solid:arrow-up-right" />
     </StyledText>
-  </>
+  </p>
 )
 
 export const needsWalletConnectionTooltip = (
-  <>Connect your wallet to access this feature.</>
+  <p>Connect your wallet to access this feature.</p>
 )
 
 export const networkLimitReachedTooltip = (
-  <>
+  <p>
     The cap has been reached for this round. Join the{" "}
     <StyledText
       variant="link"
@@ -394,18 +387,18 @@ export const networkLimitReachedTooltip = (
       <Icon name="solid:arrow-up-right" />
     </StyledText>{" "}
     to get notified when the next round starts.
-  </>
+  </p>
 )
 
 export const currentRoundUniqueWalletsTooltip = (
-  <>
+  <p>
     The total number of individual wallets that have participated in Hydro. Each
     wallet is counted once.
-  </>
+  </p>
 )
 
 export const polAvailableTooltip = (
-  <>
+  <p>
     The total ATOM available to be deployed across all bids from the Hydro
     Committee as PoL.{" "}
     <StyledText
@@ -418,11 +411,11 @@ export const polAvailableTooltip = (
       What is PoL?
       <Icon name="solid:arrow-up-right" />
     </StyledText>
-  </>
+  </p>
 )
 
 export const polDeployedTooltip = (
-  <>
+  <p>
     The total amount of PoL that has been deployed to bids over time. It is the
     aggregate amount of all past-round and Pre-Hydro deployments.{" "}
     <StyledText
@@ -435,11 +428,11 @@ export const polDeployedTooltip = (
       What is PoL?
       <Icon name="solid:arrow-up-right" />
     </StyledText>
-  </>
+  </p>
 )
 
 export const polDurationTooltip = (
-  <>
+  <p>
     The length of time the bid will receive liquidity from Hydro. Users can only
     vote for bids with a PoL Duration that matches or is shorter than your
     longest lockup period.{" "}
@@ -453,11 +446,11 @@ export const polDurationTooltip = (
       <span>Learn More</span>
       <Icon name="solid:arrow-up-right-from-square" />
     </StyledText>
-  </>
+  </p>
 )
 
 export const polRevenueTooltip = (
-  <>
+  <p>
     The total revenue generated from deployed PoL for the Cosmos Hub and Hydro,
     including rewards and tribute from funded bids.{" "}
     <StyledText
@@ -470,11 +463,11 @@ export const polRevenueTooltip = (
       What is PoL?
       <Icon name="solid:arrow-up-right" />
     </StyledText>
-  </>
+  </p>
 )
 
 export const rewardsPolRewardsColumnTooltip = (
-  <>
+  <p>
     Your share of the PoL rewards generated by this PoL deployment.{" "}
     <StyledText
       as={Link}
@@ -486,34 +479,34 @@ export const rewardsPolRewardsColumnTooltip = (
       What is PoL?
       <Icon name="solid:arrow-up-right" />
     </StyledText>
-  </>
+  </p>
 )
 
 export const rewardsYourTributeColumnTooltip = (
-  <>
+  <p>
     The tribute you earned from this bid in this round, based on your voting
     power. If a bidder added additional tributes in a round to a bid, or used
     multiple tokens as tribute, you may see multiple rows for the same bid.
-  </>
+  </p>
 )
 
 export const rewardsTotalTributeColumnTooltip = (
-  <>
+  <p>
     The tribute that was offered for this bid in the round displayed. If a
     bidder added additional tribute in a round to a bid, or used multiple tokens
     as tribute, you may see multiple rows for the same bid.
-  </>
+  </p>
 )
 
 export const rewardsYourTributeTooltip = (
-  <>
+  <p>
     The estimated USD-equivalent value of the tribute you&rsquo;ve received from
     this bid.
-  </>
+  </p>
 )
 
 export const timeLeftTooltip = (
-  <>
+  <p>
     Amount of time until the round ends. Users must vote before the end of the
     round to receive tributes.{" "}
     <StyledText
@@ -526,18 +519,18 @@ export const timeLeftTooltip = (
       <span>Learn More</span>
       <Icon name="solid:arrow-up-right" />
     </StyledText>
-  </>
+  </p>
 )
 
 export const usdDisclaimerTooltip = (
-  <>
+  <p>
     USD equivalent values are estimates and may not reflect the actual current
     value.
-  </>
+  </p>
 )
 
 export const voteThresholdTooltip = (
-  <>
+  <p>
     Bids below the minimum threshold of{" "}
     <strong>{VOTE_SHARE_THRESHOLD}% total voting power</strong> will not receive
     liquidity, and will not pay out tribute to users.{" "}
@@ -551,47 +544,47 @@ export const voteThresholdTooltip = (
       Learn more
       <Icon name="solid:arrow-up-right" />
     </StyledText>
-  </>
+  </p>
 )
 
 export const yourAggregateAprTooltip = (
-  <>
+  <p>
     The Aggregate historical APR for all past rounds, based on total votes and
     total tribute paid out to users.
-  </>
+  </p>
 )
 
 export const yourRoundAprTooltip = (
-  <>
+  <p>
     The Average APR for the previous round, based on total votes and total
     tribute paid out to users.
-  </>
+  </p>
 )
 
 export const yourTotalAtomLockedTooltip = (
-  <>
+  <p>
     Your staked ATOM locked in Hydro. The more ATOM you lock, the higher your
     voting power will be.
-  </>
+  </p>
 )
 
 export const yourTotalRewardsAllTimeTooltip = (
-  <>
+  <p>
     Based on today&rsquo;s prices, the expected aggregate USD-equivalent value
     of tribute you have accumulated across the Hydro rounds you have
     participated in.
-  </>
+  </p>
 )
 
 export const yourVotingPowerTooltip = (
-  <>
+  <p>
     Your Hydro voting power. The more power you have, the larger share of
     tributes you will receive
-  </>
+  </p>
 )
 
 export const bidDetailsPolSizeTooltip = (
-  <>
+  <p>
     The total amount of ATOM allocated to this bid as PoL during the specified
     round.{" "}
     <StyledText
@@ -604,19 +597,19 @@ export const bidDetailsPolSizeTooltip = (
       What is PoL?
       <Icon name="solid:arrow-up-right" />
     </StyledText>
-  </>
+  </p>
 )
 
 export const bidDetailsStatusTooltip = (
-  <>
+  <p>
     The current status of the liquidity deployment, such as &lsquo;Voting
     Period&rsquo;, &lsquo;Ongoing&rsquo; for active deployments,
     &lsquo;Completed&rsquo;, or &lsquo;Rejected&rsquo; for completed ones.
-  </>
+  </p>
 )
 
 export const bidDetailsMaxDeploymentAmountTooltip = (
-  <>
+  <p>
     The maximum liquidity this bid can receive is capped by the tribute offered,
     based on Hydro’s tribute floor rule.{" "}
     <StyledText
@@ -628,13 +621,13 @@ export const bidDetailsMaxDeploymentAmountTooltip = (
       <span>Learn More</span>
       <Icon name="solid:arrow-up-right" />
     </StyledText>
-  </>
+  </p>
 )
 
 export const bidDetailsVoteReceivedTooltip = (
-  <>
+  <p>
     The percentage of votes that this bid received during the specified round.
-  </>
+  </p>
 )
 
-export const globalTotalAtomLockedTooltip = <>Total ATOM locked in Hydro.</>
+export const globalTotalAtomLockedTooltip = <p>Total ATOM locked in Hydro.</p>
