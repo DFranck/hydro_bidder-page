08dcba70f73bf695ae228dc1e6774378ce80d7ca
Aaron Wright <aaronmw@gmail.com>
2025-02-14 10:29:25 -0500

    Fix neutron RPC endpoint

--- config/index.ts
+++ config/index.ts
@@ -29,7 +29,7 @@ export const endpoints = {
   },
 }
 
-export const NEUTRON_DEFAULT_RPC = "https://rpc.neutron.quokkastake.io/"
+export const NEUTRON_DEFAULT_RPC = "https://neutron-rpc.polkachu.com/"
 
 export const DEFAULT_EPOCH_LENGTH = 2628000000000000
 
