abd2e6c6127f0e80d08b41faf1e03bad49d16dd1
Aaron Wright <aaronmw@gmail.com>
2025-02-20 15:19:45 -0500

    Un-f*ck the LaTeX formula

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
-import BundleAnalyzer from "@next/bundle-analyzer"
 import Nextra from "nextra"
-
-const withBundleAnalyzer = BundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-})
 
 const withNextra = Nextra({
   theme: "nextra-theme-docs",
@@ -11,25 +6,23 @@ const withNextra = Nextra({
   latex: true,
 })
 
-const nextConfig = withBundleAnalyzer(
-  withNextra({
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "*.netlify.app",
-        },
-        {
-          protocol: "https",
-          hostname: "*.github.com",
-        },
-        {
-          protocol: "https",
-          hostname: "*.githubusercontent.com",
-        },
-      ],
-    },
-  })
-)
+const nextConfig = withNextra({
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.netlify.app",
+      },
+      {
+        protocol: "https",
+        hostname: "*.github.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.githubusercontent.com",
+      },
+    ],
+  },
+})
 
 export default nextConfig
