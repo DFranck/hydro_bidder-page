de3013857e6a99839cd33193cd6249f85351315c
Aaron Wright <aaronmw@gmail.com>
2025-02-20 12:31:41 -0500

    Move globalLockupCapacityInfo to hydroData

--- contract-apis/api/fetchExternalData.ts
+++ contract-apis/api/fetchExternalData.ts
@@ -2,30 +2,22 @@
 
 import { fetchAssetListWithPrices } from "@/contract-apis/fetchAssetListWithPrices"
 import { fetchBidDescriptionsById } from "@/contract-apis/fetchBidDescriptions"
-import { fetchGlobalLockupCapacity } from "@/contract-apis/fetchGlobalLockupCapacity"
 import { fetchNumiaBidData } from "@/contract-apis/fetchNumiaBidData"
 import { fetchNumiaMetricsData } from "@/contract-apis/fetchNumiaMetricsData"
 
 export async function fetchExternalData() {
-  const [
-    assetListWithPrices,
-    numiaData,
-    bidDescriptionsByBidId,
-    metrics,
-    globalLockupCapacityInfo,
-  ] = await Promise.all([
-    fetchAssetListWithPrices(),
-    fetchNumiaBidData(),
-    fetchBidDescriptionsById(),
-    fetchNumiaMetricsData(),
-    fetchGlobalLockupCapacity(),
-  ])
+  const [assetListWithPrices, numiaData, bidDescriptionsByBidId, metrics] =
+    await Promise.all([
+      fetchAssetListWithPrices(),
+      fetchNumiaBidData(),
+      fetchBidDescriptionsById(),
+      fetchNumiaMetricsData(),
+    ])
 
   return {
     assetListWithPrices,
     numiaData,
     bidDescriptionsByBidId,
     metrics,
-    globalLockupCapacityInfo,
   }
 }
