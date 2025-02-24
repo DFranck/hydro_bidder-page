f4ba38f0f4fea68196cb608086ddff5d25ae3490
Aaron Wright <aaronmw@gmail.com>
2025-02-24 13:15:02 -0500

    Split bid tribute APR tooltip by live and past bid

--- app/(with-backend-data)/bids/page.tsx
+++ app/(with-backend-data)/bids/page.tsx
@@ -18,7 +18,7 @@ import { Tooltip } from "@/components/Tooltip"
 import {
   bidTablesFirstColumnTooltips,
   currentVoteShareTooltip,
-  metricsTributeAprColumnTooltip,
+  liveBidTributeAprColumnTooltip,
   metricsTributeColumnTooltip,
   polDurationTooltip,
   VOTE_SHARE_THRESHOLD,
@@ -161,7 +161,7 @@ export default function BidsPage() {
             tipContents={
               !isTokenBased
                 ? metricsTributeColumnTooltip
-                : metricsTributeAprColumnTooltip
+                : liveBidTributeAprColumnTooltip
             }
           >
             <div className="flex items-center gap-1">
