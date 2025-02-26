7543ad90be563c56acf96f54b8b067b70b6b5121
Aaron Wright <aaronmw@gmail.com>
2025-02-26 10:24:08 -0500

    Fix APR

--- lib/isDeepEqual.ts
+++ lib/isDeepEqual.ts
@@ -1,9 +1,14 @@
-export function isDeepEqual<T>(a: T, b: T): boolean {
+export function isDeepEqual<T>(a: T, b: T, visited = new WeakMap()): boolean {
   if (a === b) return true
 
   if (typeof a !== "object" || typeof b !== "object" || !a || !b) {
     return false
   }
+
+  if (visited.has(a as object)) {
+    return visited.get(a as object) === b
+  }
+  visited.set(a as object, b)
 
   const keysA = Object.keys(a as object)
   const keysB = Object.keys(b as object)
@@ -13,6 +18,6 @@ export function isDeepEqual<T>(a: T, b: T): boolean {
   }
 
   return keysA.every((key) => {
-    return isDeepEqual((a as any)[key], (b as any)[key])
+    return isDeepEqual((a as any)[key], (b as any)[key], visited)
   })
 }
