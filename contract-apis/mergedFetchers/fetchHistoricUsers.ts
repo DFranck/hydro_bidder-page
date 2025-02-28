85ead837556ca25a17c30d55bce2d82d81b71957
Lucas García de Viedma Pérez <72617878+Lucasgvdii@users.noreply.github.com>
2025-02-28 13:35:03 +0100

    Add new endpoint to query hydro users and make numia-lockups query to do 1 single request

---
+++ contract-apis/mergedFetchers/fetchHistoricUsers.ts
@@ -0,0 +1,24 @@
+
+export async function fetchHistoricUsers(
+): Promise<{ users : string[] }> {
+
+  const response = await fetch(
+    `${process.env.NUMIA_USERS_ENDPOINT}`, 
+    {
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${process.env.NUMIA_COSMOS_HYDRO_APP_API_KEY}`,
+      },
+    }
+  )
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch numia users data: ${response.statusText}`)
+  }
+
+  // Clean up the response
+  const responseJson = await response.json()
+  const users = responseJson[0];
+  return users;
+
+}
