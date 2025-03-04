b91fdbc2e897280ba453253bb552f9d77dc9ec67
Philip Offtermatt <p.offtermatt@gmail.com>
2025-03-04 08:44:53 +0100

    Change node endpoints

--- config/index.ts
+++ config/index.ts
@@ -16,7 +16,7 @@ export const endpointsOnClient = {
     rest: ["https://rest.cosmos.directory/neutron/"],
   },
   cosmoshub: {
-    rpc: ["https://cosmos-rpc.publicnode.com/"],
+    rpc: ["https://rpc.cosmos.directory/cosmoshub"],
     rest: ["https://cosmos-rest.publicnode.com/"],
   },
 }
