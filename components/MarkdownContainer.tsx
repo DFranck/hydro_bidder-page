ceab9664a65f28221736846afeff5c96b16c003c
Aaron Wright <aaronmw@gmail.com>
2025-03-06 10:39:56 -0500

    Add tables to markdown

--- components/MarkdownContainer.tsx
+++ components/MarkdownContainer.tsx
@@ -24,6 +24,17 @@ export function MarkdownContainer({
           prose-code:text-palette-beige
           prose-ol:text-white
           prose-li:text-white
+          prose-table:border-collapse
+          prose-table:overflow-hidden
+          prose-table:rounded-md
+          prose-table:border
+          prose-thead:bg-palette-beige/10
+          prose-th:border
+          prose-th:px-3
+          prose-th:py-1
+          prose-td:border
+          prose-td:px-3
+          prose-td:py-1
           [&_a:hover]:text-palette-green
         `,
         className
