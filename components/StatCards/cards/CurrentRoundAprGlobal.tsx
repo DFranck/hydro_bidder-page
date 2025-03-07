6e3a2eaeb1c2226afb57b2a4cbc30e71d4e619a6
Aaron Wright <aaronmw@gmail.com>
2025-03-07 11:55:43 -0500

    NaN -> 0

--- components/StatCards/cards/CurrentRoundAprGlobal.tsx
+++ components/StatCards/cards/CurrentRoundAprGlobal.tsx
@@ -14,12 +14,16 @@ export function CurrentRoundAprGlobal() {
   )
 
   // get the bids that have a points-based tribute and filter those out for the average
-  const bidsWithoutPointsTributes = bidsInRound.filter((bid) => bid.tributes.some((tribute) => tribute.isTokenBased))
+  const bidsWithoutPointsTributes = bidsInRound.filter((bid) =>
+    bid.tributes.some((tribute) => tribute.isTokenBased)
+  )
 
   const totalTributeAprMin = sumBy(bidsWithoutPointsTributes, "tributeAprMin")
   const totalTributeAprMax = sumBy(bidsWithoutPointsTributes, "tributeAprMax")
   const averageTributeApr =
-    (totalTributeAprMin + totalTributeAprMax) / 2 / bidsWithoutPointsTributes.length
+    (totalTributeAprMin + totalTributeAprMax) /
+    2 /
+    bidsWithoutPointsTributes.length
 
   return (
     <StatCard
@@ -33,7 +37,7 @@ export function CurrentRoundAprGlobal() {
         </Tooltip>
       }
       subTitle={`Pilot Round ${currentRoundId + 1}`}
-      value={averageTributeApr.toLocaleString("en-US", {
+      value={(averageTributeApr || 0).toLocaleString("en-US", {
         style: "percent",
       })}
     />
