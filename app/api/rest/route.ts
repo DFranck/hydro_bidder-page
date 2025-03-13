e6eb6b14932bf03f1990ce757225ddae031f14e1
Aaron Wright <aaronmw@gmail.com>
2025-03-13 12:50:14 -0400

    Remove unused file

--- app/api/rest/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server"
-
-export async function POST(req: Request) {
-  const { address } = await req.json()
-  const url = `https://api.neutron.org/v1/accounts/${address}`
-  const response = await fetch(url)
-  const data = await response.json()
-  return NextResponse.json(data)
-}
