dfd8a483f4b70fb3ea84e2d8f231e3647ab0fa6e
Aaron Wright <aaronmw@gmail.com>
2025-03-13 13:06:23 -0400

    Fix denom trace endpoint

--- app/(with-backend-data)/lock-atom/transactions/fetchDenomTrace.ts
+++ app/(with-backend-data)/lock-atom/transactions/fetchDenomTrace.ts
@@ -1,14 +1,25 @@
-export const fetchDenomTrace = async (
-  balance: {
-    denom: string
-    amount: string
-  },
-  restEndpoint: string
-) => {
+"use server"
+
+import { getEndpoints } from "@/config"
+
+export async function fetchDenomTrace(balance: {
+  denom: string
+  amount: string
+}) {
   if (balance.denom?.startsWith("ibc/")) {
     try {
-      const url = `${restEndpoint}ibc/apps/transfer/v1/denom_traces/${balance.denom}`
-      const denomTraceResponse = await fetch(url).then((res) => res.json())
+      const endpoint = getEndpoints({
+        environmentVariables: {
+          NUMIA_COSMOS_HYDRO_APP_API_KEY:
+            process.env.NUMIA_COSMOS_HYDRO_APP_API_KEY!,
+        },
+      }).neutron.rpc[0]
+
+      const url = `${endpoint.url.replace(/\/$/, "")}/ibc/apps/transfer/v1/denom_traces/${balance.denom}`
+
+      const denomTraceResponse = await fetch(url, {
+        headers: endpoint.headers,
+      }).then((res) => res.json())
       const baseDenom = denomTraceResponse.denom_trace.base_denom
 
       if (baseDenom?.startsWith("cosmosvaloper")) {
