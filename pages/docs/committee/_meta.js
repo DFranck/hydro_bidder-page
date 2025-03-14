520e7ecaae0cd7b2efb0c879d63b8b04d2de8889
dcgr11 <delisse@hydrolabs.team>
2025-03-14 06:02:58 -0600

    Split Guidelines page

--- pages/docs/committee/_meta.js
+++ pages/docs/committee/_meta.js
@@ -2,8 +2,20 @@ const meta = {
   "committee-members": {
     title: "Members",
   },
-  "hydro-guidelines": {
-    title: "Guidelines",
+  "risk-management": {
+    title: "Risk Management",
+  },
+  "deployment-performance": {
+    title: "Deployment Performance",
+  },
+  "security": {
+    title: "Security",
+  },
+  "auction-performance": {
+    title: "Auction Performance",
+  },
+  "deployment-process": {
+    title: "Deployment Process",
   },
   "coi-policy": {
     title: "COI Policy",
