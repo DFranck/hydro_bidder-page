0865af39f71a99a4f5b6bdf2799cfd9685016013
Aaron Wright <aaronmw@gmail.com>
2025-03-21 10:02:21 -0400

    Remove cache-busting route

--- app/api/bust-a-cache/route.ts
+++
@@ -1,6 +0,0 @@
-import { clearNetlifyCache } from "@/lib/clearNetlifyCache"
-
-export async function GET() {
-  await clearNetlifyCache()
-  return new Response("Cache cleared successfully!", { status: 200 })
-}
