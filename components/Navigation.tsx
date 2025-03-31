c6b119c65f446a6f85085bb772a81159bead0928
Aaron Wright <aaronmw@gmail.com>
2025-03-31 19:04:46 -0400

    Fix resources nav when hidden

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -291,7 +291,9 @@ export default function Navigation() {
                     "lg:bg-palette-text",
                     "lg:shadow-2xl",
                     "lg:opacity-0",
-                    "lg:group-has-[:focus-within]:opacity-100"
+                    "lg:pointer-events-none",
+                    "lg:group-has-[:focus-within]:opacity-100",
+                    "lg:group-has-[:focus-within]:pointer-events-auto",
                   )}
                 >
                   {subMenuItems.map(
