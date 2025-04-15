c8185496e5dc4bec55b24b23bc158bdaf2d9f268
Philip Offtermatt <p.offtermatt@gmail.com>
2025-04-15 16:27:38 +0200

    Replace column title

--- app/(with-backend-data)/metrics/[[...roundNumber]]/buildColumns.tsx
+++ app/(with-backend-data)/metrics/[[...roundNumber]]/buildColumns.tsx
@@ -83,7 +83,7 @@ export function buildColumns(
       label: (
         <Tooltip tipContents={metricsPolRewardsColumnTooltip}>
           <div className="flex items-center gap-1">
-            PoL APR
+            Total APR
             <Icon name="circle-info" />
           </div>
         </Tooltip>
