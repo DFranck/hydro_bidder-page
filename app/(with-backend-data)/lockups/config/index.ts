df9e67d9db3f3dd0d2f9848d7ef2c190979b1409
clarencepenz <clarencedespenz@gmail.com>
2025-07-30 17:08:34 +0100

    chore: update config for allowed nft sizes

--- app/(with-backend-data)/lockups/config/index.ts
+++ app/(with-backend-data)/lockups/config/index.ts
@@ -1,7 +1,7 @@
 import { TOKEN_DENOMS } from "@/lib/tokenDenoms"
 
 export const NFT_SIZES =
-  process.env.NEXT_PUBLIC_ENV === "prod"
+  process.env.NEXT_PUBLIC_ENVIRONMENT === "production"
     ? [25, 50, 100, 250, 500, 1000]
     : [0.2, 0.62, 1.3, 1.98, 2.74, 4.26]
 
