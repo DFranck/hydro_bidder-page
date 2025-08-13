88a41857c8a462ca4f151bed899e620d27c75f0f
Nikola Bogdanovic <nikolabogdanovic@informal.systems>
2025-08-13 14:53:37 +0200

    Changed supabase file name

--- app/api/tweak-nft-store/route.ts
+++ app/api/tweak-nft-store/route.ts
@@ -3,6 +3,7 @@
 import { ListingMutationAPIResponse } from "@/app/(with-backend-data)/lockups/marketplace/types"
 import { Listing } from "@/app/ts_types/MarketplaceBase.types"
 import { getMarketplaceQueryClient } from "@/contract-apis/getClient"
+import { getSupabaseNamespacedFilename } from "@/lib/getSupabaseNamespacedFilename"
 import { revalidateTag } from "@/lib/revalidateTag"
 import { supabase } from "@/lib/supabase"
 import { NextRequest, NextResponse } from "next/server"
@@ -16,7 +17,7 @@ export async function POST(req: NextRequest) {
     if (!collection || !tokenId || !action) {
       return NextResponse.json(
         { error: "Missing collection or tokenId or action" },
-        { status: 400 },
+        { status: 400 }
       )
     }
 
@@ -30,9 +31,7 @@ export async function POST(req: NextRequest) {
     }
 
     let listings: Listing[] = []
-// TODO : Replace with the correct filename beor PR
-    // const filename = getSupabaseNamespacedFilename(LISTINGS_FILE)
-    const filename = "moonkitt-dev--" + LISTINGS_FILE
+    const filename = getSupabaseNamespacedFilename(LISTINGS_FILE)
     try {
       const { data, error } = await supabase.storage
         .from(STORAGE_BUCKET)
@@ -47,7 +46,7 @@ export async function POST(req: NextRequest) {
     let operation: ListingMutationAPIResponse["operation"]
     let listingToReturn: Listing | null = latestListing
     const findIdx = listings.findIndex(
-      (l) => l.collection === collection && l.token_id === tokenId,
+      (l) => l.collection === collection && l.token_id === tokenId
     )
 
     // State mutation
@@ -57,7 +56,7 @@ export async function POST(req: NextRequest) {
         if (!latestListing) {
           return NextResponse.json(
             { success: false, error: "Missing listing for upsert" },
-            { status: 400 },
+            { status: 400 }
           )
         }
         if (findIdx >= 0) {
@@ -79,12 +78,12 @@ export async function POST(req: NextRequest) {
               success: false,
               error: "Listing should not exist anymore after removal",
             },
-            { status: 400 },
+            { status: 400 }
           )
         }
         if (findIdx < 0) {
           console.warn(
-            "[API] Listing not found for removal — treating as noop OK",
+            "[API] Listing not found for removal — treating as noop OK"
           )
           return NextResponse.json({
             success: true,
@@ -102,7 +101,7 @@ export async function POST(req: NextRequest) {
       default:
         return NextResponse.json(
           { success: false, error: "Invalid action" },
-          { status: 400 },
+          { status: 400 }
         )
     }
 
@@ -117,7 +116,7 @@ export async function POST(req: NextRequest) {
       console.error("[API] Failed to upload to Supabase:", uploadError)
       return NextResponse.json(
         { error: "Critical error: failed to write listing to Supabase" },
-        { status: 500 },
+        { status: 500 }
       )
     }
 
@@ -131,7 +130,7 @@ export async function POST(req: NextRequest) {
     console.error("[API] ERROR:", e)
     return NextResponse.json(
       { error: e.message || e.toString() },
-      { status: 500 },
+      { status: 500 }
     )
   }
 }
